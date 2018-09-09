bd0b3d98525062d1625fb3fcee25a9d63a70e105
PeterStampfli <stampfli@macquality.ch>
2018-09-09 20:53:34 +0200

    small12 finished untested

--- librarySource/small12.js
+++ librarySource/small12.js
@@ -14,19 +14,17 @@ var small12 = {};
 
     // starting the iteration with a dodecagon
     small12.start = function() {
-        const side = 2;
+        const side = 3;
         const zero = new Vector2(0, 0);
         const bottom = new Vector2(side, 0);
         const top = bottom.clone().rotate(small12.angle);
         const right = new Vector2(side * (1 + small12.rt32), side * 0.5);
         const mLow = new Vector2(right.x, 0);
         const mHigh = mLow.clone().rotate(small12.angle);
-        for (var i = 0; i < 12; i++) {
-            small12.rhomb(0, zero, right);
-            small12.triangleC(0, true, top, right, mHigh);
-            //  small12.triangleC(0,false,bottom,right,mLow);
-
-
+        for (var i = 0; i < 1; i++) {
+            //  small12.rhomb(0, zero, right);
+            small12.triangleA(0, true, top, right, mHigh);
+            //  small12.triangleA(0,false,bottom,right,mLow);
             bottom.rotate(small12.angle);
             top.rotate(small12.angle);
             right.rotate(small12.angle);
@@ -34,7 +32,6 @@ var small12 = {};
             mHigh.rotate(small12.angle);
         }
     };
-
 
     small12.rhomb = function(ite, left, right) {
         // create the corner points
@@ -58,9 +55,80 @@ var small12 = {};
                 const centerLeftRatio = 1 / (1 + Math.sqrt(2) * 2 * Fast.cos(Math.PI / 12));
                 const centerLeft = Vector2.lerp(center, centerLeftRatio, left);
                 const centerRight = Vector2.lerp(center, centerLeftRatio, right);
+                // iterating tiles
+                small12.rhomb(ite + 1, left, centerLeft);
+                small12.rhomb(ite + 1, right, centerRight);
+                small12.triangleA(ite + 1, false, leftBottom, centerLeft, bottomLeft);
+                small12.triangleA(ite + 1, true, leftTop, centerLeft, topLeft);
+                small12.triangleA(ite + 1, true, rightBottom, centerRight, bottomRight);
+                small12.triangleA(ite + 1, false, rightTop, centerRight, topRight);
+                small12.triangleC(ite + 1, true, bottom, centerLeft, bottomLeft);
+                small12.triangleC(ite + 1, false, top, centerLeft, topLeft);
+                small12.triangleC(ite + 1, false, bottom, centerRight, bottomRight);
+                small12.triangleC(ite + 1, true, top, centerRight, topLeft);
+                small12.quarterSquare(ite + 1, bottom, center);
+                small12.quarterSquare(ite + 1, top, center);
+                small12.quarterSquare(ite + 1, centerLeft, center);
+                small12.quarterSquare(ite + 1, centerRight, center);
             } else {
                 // end iteration, make an image tile
                 imageTiles.addSymmetricParallelogram(small12.angle, left, right);
+            }
+        }
+    };
+
+    small12.triangleA = function(ite, counterclockwise, a, b, c) {
+        if (iterateTiling.structure[ite].isNewPolygon(a, b, c)) {
+            if (ite < iterateTiling.maxIterations) {
+                const bc = Vector2.lerp(c, small12.ratio, b);
+                const ca = Vector2.lerp(c, 1 / (3 + small12.rt3), a);
+                const center = Vector2.difference(bc, c).add(ca);
+                const ac = Vector2.lerp(a, 2 / (3 + small12.rt3), c);
+                const ba = Vector2.lerp(b, small12.ratio, a);
+                const ab = Vector2.lerp(a, small12.rt32 * small12.ratio, b);
+                // iterating tiles
+                // same for all
+                small12.quarterSquare(ite + 1, center, c);
+                small12.triangleC(ite + 1, counterclockwise, ac, center, ca);
+                small12.triangleB(ite + 1, !counterclockwise, b, center, bc);
+                // different
+                small12.rhomb(ite + 1, b, ac);
+                small12.triangleA(ite + 1, counterclockwise, ba, ac, ab);
+                small12.triangleB(ite + 1, !counterclockwise, a, ac, ab);
+            } else {
+                if (counterclockwise) {
+                    imageTiles.addSymmetricPolygon(3, a, b);
+                } else {
+                    imageTiles.addSymmetricPolygon(3, b, a);
+                }
+            }
+        }
+    };
+
+    small12.triangleB = function(ite, counterclockwise, a, b, c) {
+        if (iterateTiling.structure[ite].isNewPolygon(a, b, c)) {
+            if (ite < iterateTiling.maxIterations) {
+                const bc = Vector2.lerp(c, small12.ratio, b);
+                const ca = Vector2.lerp(c, 1 / (3 + small12.rt3), a);
+                const center = Vector2.difference(bc, c).add(ca);
+                const ac = Vector2.lerp(a, 2 / (3 + small12.rt3), c);
+                const ba = Vector2.lerp(b, small12.ratio, a);
+                const ab = Vector2.lerp(a, small12.rt32 * small12.ratio, b);
+                // iterating tiles
+                // same for all
+                small12.quarterSquare(ite + 1, center, c);
+                small12.triangleC(ite + 1, counterclockwise, ac, center, ca);
+                small12.triangleB(ite + 1, !counterclockwise, b, center, bc);
+                // different
+                small12.rhomb(ite + 1, a, center);
+                small12.triangleA(ite + 1, !counterclockwise, ab, center, ba);
+                small12.triangleC(ite + 1, counterclockwise, b, center, ba);
+            } else {
+                if (counterclockwise) {
+                    imageTiles.addSymmetricPolygon(3, a, b);
+                } else {
+                    imageTiles.addSymmetricPolygon(3, b, a);
+                }
             }
         }
     };
@@ -71,10 +139,19 @@ var small12 = {};
                 const bc = Vector2.lerp(c, small12.ratio, b);
                 const ca = Vector2.lerp(c, 1 / (3 + small12.rt3), a);
                 const center = Vector2.difference(bc, c).add(ca);
-                const ac = Vector2.lerp(a, 2 / (1 + small12.rt3), c);
+                const ac = Vector2.lerp(a, 2 / (3 + small12.rt3), c);
                 const ba = Vector2.lerp(b, small12.ratio, a);
                 const ab = Vector2.lerp(a, small12.rt32 * small12.ratio, b);
                 const m = Vector2.center(a, b);
+                // iterating tiles
+                // same for all
+                small12.quarterSquare(ite + 1, center, c);
+                small12.triangleC(ite + 1, counterclockwise, ac, center, ca);
+                small12.triangleB(ite + 1, !counterclockwise, b, center, bc);
+                // different
+                small12.triangleB(ite + 1, !counterclockwise, a, ac, ab);
+                small12.quarterSquare(ite + 1, ac, m);
+                small12.quarterSquare(ite + 1, center, m);
             } else {
                 if (counterclockwise) {
                     imageTiles.addSymmetricPolygon(3, a, b);
@@ -86,8 +163,8 @@ var small12 = {};
     };
 
     small12.quarterSquare = function(ite, corner, center) {
-        const a = Vector2.difference(corner, center).scale(Math.sqrt(0.5)).rotate(Math.pi / 4).add(center);
-        const b = Vector2.difference(corner, center).scale(Math.sqrt(0.5)).rotate(-Math.pi / 4).add(center);
+        const a = Vector2.difference(corner, center).scale(Math.sqrt(0.5)).rotate(Math.PI / 4).add(center);
+        const b = Vector2.difference(corner, center).scale(Math.sqrt(0.5)).rotate(-Math.PI / 4).add(center);
         if (iterateTiling.structure[ite].isNewPolygon(center, b, corner, a)) {
             if (ite < iterateTiling.maxIterations) {
                 const aCorner = Vector2.lerp(a, small12.ratio, corner);
@@ -96,19 +173,20 @@ var small12 = {};
                 const bCenter = Vector2.lerp(b, small12.ratio, center);
                 const ab = Vector2.difference(aCenter, a).add(aCorner);
                 const ba = Vector2.difference(bCenter, b).add(bCorner);
+                // iterating tiles
+                small12.rhomb(ite + 1, corner, center);
+                small12.quarterSquare(ite + 1, ab, a);
+                small12.quarterSquare(ite + 1, ba, b);
+                small12.triangleB(ite + 1, false, corner, ab, aCorner);
+                small12.triangleB(ite + 1, true, center, ab, aCenter);
+                small12.triangleB(ite + 1, false, center, ba, bCenter);
+                small12.triangleB(ite + 1, true, corner, ba, bCorner);
             } else {
                 // corner of the full triangle
                 a.scale(2).sub(corner);
-                if (counterclockwise) {
-                    imageTiles.addSymmetricPolygon(4, corner, a);
-                } else {
-                    imageTiles.addSymmetricPolygon(4, a, corner);
-                }
+                imageTiles.addSymmetricPolygon(4, corner, a);
             }
         }
-
     };
-
-
 
 }());
