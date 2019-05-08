e2d6541c0fce2a73cbfa04659d4ef05b3a2577d1
PeterStampfli <stampfli@macquality.ch>
2019-05-08 21:06:23 +0200

    refactoring image, interactive

--- geometricolor/interaction2019.js
+++ geometricolor/interaction2019.js
@@ -1,53 +1,69 @@
 /* jshint esversion:6 */
 
-if (Layout.isLandscape()) {
+/**
+ * layout for a page with interactive image and navigation buttons
+ * @method Layout.interactive
+ * @param {String} prev - url of page to go back, "" for home page
+ * @param {String} next - url of page to go to next, "" for home page
+ * @return true if landscape
+ */
+Layout.interactive = function(prev, next) {
+    if (Layout.isLandscape()) {
 
-    Layout.setup("setup.html", "triangles.html");
-    Layout.createOpenImage();
-            Layout.navigationAtRight();
+        Layout.setup(prev, next);
+
+        Layout.createOpenImage();
+        Layout.navigationAtRight();
 
 
-    // setting up the images
-    // output image
-    Make.createOutputImage("outputCanvas");
-    Make.outputImage.setDivDimensions(window.innerHeight, window.innerHeight);
-    Make.outputImage.setDivPosition(0, 0);
-    //  Make.outputImage.stopZoom();
-    //  Make.outputImage.stopShift();
-    Draw.setOutputImage(Make.outputImage);
-    // hidden control images
-    Make.createControlImage("controlCanvas", false);
-    Make.createArrowController("arrowController", false);
-    Make.createMap(); // needs the images
-    Make.setOutputSize(window.innerHeight);
-
-    Layout.createTextDiv();
+        // setting up the images
+        // output image
+        Make.createOutputImage("outputCanvas");
+        Make.outputImage.setDivDimensions(window.innerHeight, window.innerHeight);
+        Make.outputImage.setDivPosition(0, 0);
+        //  Make.outputImage.stopZoom();
+        //  Make.outputImage.stopShift();
+        Draw.setOutputImage(Make.outputImage);
+        // hidden control images
+        Make.createControlImage("controlCanvas", false);
+        Make.createArrowController("arrowController", false);
+        Make.createMap(); // needs the images
+        Make.setOutputSize(window.innerHeight);
+        Layout.createTextDiv();
 
         if (DOM.idExists("show")) {
 
-    // choose between showing the structure or the image
-    let showSelect = new Select("show");
+            // choose between showing the structure or the image
+            let showSelect = new Select("show");
 
-    showSelect.addOption("image",
-        function() {
-            Make.showingInputImage = true;
-            Make.draw = function() {
-                Make.drawImage();
-            };
-            Make.updateOutputImage();
-        });
+            showSelect.addOption("image",
+                function() {
+                    Make.showingInputImage = true;
+                    Make.draw = function() {
+                        Make.drawImage();
+                    };
+                    Make.updateOutputImage();
+                });
 
-    showSelect.addOption("structure",
-        function() {
-            console.log("structure");
-            Make.showingInputImage = false;
-            Make.clearControlImage();
-            Make.draw = function() {
-                Make.map.drawStructure();
-            };
-            Make.updateOutputImage();
-        });
+            showSelect.addOption("structure",
+                function() {
+                    console.log("structure");
+                    Make.showingInputImage = false;
+                    Make.clearControlImage();
+                    Make.draw = function() {
+                        Make.map.drawStructure();
+                    };
+                    Make.updateOutputImage();
+                });
         }
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+if (Layout.interactive("setup.html", "triangles.html")) {
 
     let setKButton = Layout.createNumberButton("n");
     setKButton.setRange(2, 10000);
@@ -89,4 +105,5 @@ if (Layout.isLandscape()) {
     Make.initializeMap();
 
     Make.readImageWithFilePathAtSetup("guard.jpg");
+
 }
--- geometricolor/layout.js
+++ geometricolor/layout.js
@@ -11,6 +11,8 @@ var Layout = {};
 basicUI = {};
 basicUI.layout = function() {};
 
+Make.imageQuality = "high";
+
 
 (function() {
     "use strict";
@@ -280,9 +282,9 @@ basicUI.layout = function() {};
             const canvas = document.getElementById("canvasId");
             canvas.width = window.innerHeight;
             canvas.height = window.innerHeight;
-            canvas.style.position="absolute"
-            canvas.style.top="0px";
-            canvas.style.left="0px";
+            canvas.style.position = "absolute";
+            canvas.style.top = "0px";
+            canvas.style.left = "0px";
             const context = canvas.getContext("2d");
 
             /**
@@ -299,34 +301,34 @@ basicUI.layout = function() {};
             Layout.drawImage = function() {};
         }
     };
-    
-    
-/**
- * layout for a page with four still images and navigation buttons
- * @method Layout.fourImages
- * @param {String} prev - url of page to go back, "" for home page
- * @param {String} next - url of page to go to next, "" for home page
- */
-Layout.fourImages = function(prev, next) {
-    if (Layout.isLandscape()) {
-        Layout.setup(prev, next);
-        Layout.activateFontSizeChanges();
-        Layout.setFontSizes();
-        Layout.navigationAtRight();
-        Layout.createTextDiv();
-        DOM.create("canvas", "canvasId", "body");
-        const canvas = document.getElementById("canvasId");
-        canvas.width = window.innerHeight;
-        canvas.height = window.innerHeight;
-             canvas.style.position="absolute"
-            canvas.style.top="0px";
-            canvas.style.left="0px";
-        const context = canvas.getContext("2d");
-        const size2 = window.innerHeight / 2;
-        const size = window.innerHeight;
-        var image0, image1, image2, image3;
 
-         /**
+
+    /**
+     * layout for a page with four still images and navigation buttons
+     * @method Layout.fourImages
+     * @param {String} prev - url of page to go back, "" for home page
+     * @param {String} next - url of page to go to next, "" for home page
+     */
+    Layout.fourImages = function(prev, next) {
+        if (Layout.isLandscape()) {
+            Layout.setup(prev, next);
+            Layout.activateFontSizeChanges();
+            Layout.setFontSizes();
+            Layout.navigationAtRight();
+            Layout.createTextDiv();
+            DOM.create("canvas", "canvasId", "body");
+            const canvas = document.getElementById("canvasId");
+            canvas.width = window.innerHeight;
+            canvas.height = window.innerHeight;
+            canvas.style.position = "absolute";
+            canvas.style.top = "0px";
+            canvas.style.left = "0px";
+            const context = canvas.getContext("2d");
+            const size2 = window.innerHeight / 2;
+            const size = window.innerHeight;
+            var image0, image1, image2, image3;
+
+            /**
              * drawing a four images, for Layout.fourImages
              * @method Layout.drawImages
              * @param {String} path0
@@ -334,52 +336,52 @@ Layout.fourImages = function(prev, next) {
              * @param {String} path2
              * @param {String} path3
              */
-        Layout.drawImages = function(path0, path1, path2, path3) {
-            image0 = Layout.loadImage(path0, function() {
-                context.drawImage(image0, 0, 0, size2, size2);
-            });
-            image1 = Layout.loadImage(path1, function() {
-                context.drawImage(image1, size2, 0, size2, size2);
-            });
-            image2 = Layout.loadImage(path2, function() {
-                context.drawImage(image2, 0, size2, size2, size2);
-            });
-            image3 = Layout.loadImage(path3, function() {
-                context.drawImage(image3, size2, size2, size2, size2);
-            });
-        };
-        let showAll = true;
+            Layout.drawImages = function(path0, path1, path2, path3) {
+                image0 = Layout.loadImage(path0, function() {
+                    context.drawImage(image0, 0, 0, size2, size2);
+                });
+                image1 = Layout.loadImage(path1, function() {
+                    context.drawImage(image1, size2, 0, size2, size2);
+                });
+                image2 = Layout.loadImage(path2, function() {
+                    context.drawImage(image2, 0, size2, size2, size2);
+                });
+                image3 = Layout.loadImage(path3, function() {
+                    context.drawImage(image3, size2, size2, size2, size2);
+                });
+            };
+            let showAll = true;
 
-        canvas.onclick = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            const x = Math.floor(2 * event.pageX / window.innerHeight);
-            const y = Math.floor(2 * event.pageY / window.innerHeight);
-            const i = x + 2 * y;
-            if (!showAll) {
-                context.drawImage(image0, 0, 0, size2, size2);
-                context.drawImage(image1, size2, 0, size2, size2);
-                context.drawImage(image2, 0, size2, size2, size2);
-                context.drawImage(image3, size2, size2, size2, size2);
-                showAll = true;
-            } else if (i === 0) {
-                context.drawImage(image0, 0, 0, size, size);
-                showAll = false;
-            } else if (i === 1) {
-                context.drawImage(image1, 0, 0, size, size);
-                showAll = false;
-            } else if (i === 2) {
-                context.drawImage(image2, 0, 0, size, size);
-                showAll = false;
-            } else if (i === 3) {
-                context.drawImage(image3, 0, 0, size, size);
-                showAll = false;
-            }
-        };
-    } else {
-        Layout.drawImages = function() {};
-    }
-};
+            canvas.onclick = function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                const x = Math.floor(2 * event.pageX / window.innerHeight);
+                const y = Math.floor(2 * event.pageY / window.innerHeight);
+                const i = x + 2 * y;
+                if (!showAll) {
+                    context.drawImage(image0, 0, 0, size2, size2);
+                    context.drawImage(image1, size2, 0, size2, size2);
+                    context.drawImage(image2, 0, size2, size2, size2);
+                    context.drawImage(image3, size2, size2, size2, size2);
+                    showAll = true;
+                } else if (i === 0) {
+                    context.drawImage(image0, 0, 0, size, size);
+                    showAll = false;
+                } else if (i === 1) {
+                    context.drawImage(image1, 0, 0, size, size);
+                    showAll = false;
+                } else if (i === 2) {
+                    context.drawImage(image2, 0, 0, size, size);
+                    showAll = false;
+                } else if (i === 3) {
+                    context.drawImage(image3, 0, 0, size, size);
+                    showAll = false;
+                }
+            };
+        } else {
+            Layout.drawImages = function() {};
+        }
+    };
 
 
 }());
