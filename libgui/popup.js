51d02f70f7a194cfce72c844b0be86c8e7f69005
PeterStampfli <stampfli@macquality.ch>
2019-12-01 15:02:10 +0100

    popup with boundary and morer styles

--- libgui/popup.js
+++ libgui/popup.js
@@ -39,11 +39,17 @@ Popup.defaultDesign = {
     fontSize: 18,
     textColor: "#444444",
     backgroundColor: "#ffffaa",
+    padding: 10,
+    border: "solid",
+    borderWidth: 10,
+    borderColor: "yellow",
     borderRadius: 10,
     shadowWidth: 5,
     shadowBlur: 10,
+    shadowRed: 0,
+    shadowGreen: 0,
+    shadowBlue: 0,
     shadowAlpha: 0.7,
-    padding: 10,
     zIndex: 20,
     position: "center",
     horizontalShift: 0
@@ -72,9 +78,9 @@ Popup.prototype.corner = function(horizontal, vertical) {
     this.theDiv.style.bottom = "";
     this.theDiv.style.right = "";
     this.theDiv.style.left = "";
-    let shadowWidth = this.design.shadowBlur + this.design.shadowWidth;
-    this.theDiv.style[horizontal] = shadowWidth + this.design.horizontalShift + "px";
-    this.theDiv.style[vertical] = shadowWidth + "px";
+    let totalShadowWidth = this.design.shadowBlur + this.design.shadowWidth;
+    this.theDiv.style[horizontal] = totalShadowWidth + this.design.horizontalShift + "px";
+    this.theDiv.style[vertical] = totalShadowWidth + "px";
     this.theDiv.style.transform = "";
 };
 
@@ -89,7 +95,7 @@ Popup.prototype.resize = function() {
     if (noShow) {
         this.open();
     }
-    const shadowWidth = this.design.shadowBlur + this.design.shadowWidth;
+    let totalShadowWidth = this.design.shadowBlur + this.design.shadowWidth;
     this.theDiv.style.overflowY = "hidden";
     // this gives the correct result if there is no scroll bar
     // the width does not include the padding
@@ -97,13 +103,13 @@ Popup.prototype.resize = function() {
     this.theDiv.style.height = ""; // makes that height style property vanishes
     // maximum available height for the div, without shadow, but including its padding
     // clientHeight includes padding of the parent, here it is zero
-    const maxheight = document.documentElement.clientHeight - 2 * shadowWidth;
+    const maxheight = document.documentElement.clientHeight - 2 * totalShadowWidth;
     // get the total height relevant for layout, including padding and border (without shadow!)
     const divHeight = this.theDiv.offsetHeight;
     if (divHeight > maxheight) {
         // attention - height is inner, does not include padding or border
         // limit the height, such that together with shadows the popup fills the entire available height
-        this.theDiv.style.height = maxheight - 2 * this.design.padding + "px";
+        this.theDiv.style.height = maxheight - 2 * this.design.padding - 2 * this.design.borderWidth + "px";
         // a real scrolling appears only if the bottom padding has vanished
         if (divHeight - this.design.padding > maxheight) {
             this.theDiv.style.overflowY = "scroll";
@@ -126,12 +132,13 @@ Popup.prototype.resize = function() {
  * @param {...Object} newStyle - with parameter values that have to change, optional
  */
 Popup.prototype.setStyle = function(newStyle) {
+    const design = this.design;
     for (var i = 0; i < arguments.length; i++) {
         if (typeof arguments[i] === "object") {
-            Object.assign(this.design, arguments[i]);
+            Object.assign(design, arguments[i]);
         }
     }
-    switch (this.design.position) {
+    switch (design.position) {
         case "center":
             this.center();
             break;
@@ -148,17 +155,20 @@ Popup.prototype.setStyle = function(newStyle) {
             this.corner("right", "bottom");
             break;
     }
-    this.theDiv.style.zIndex = this.design.zIndex;
-    this.theDiv.style.backgroundColor = this.design.backgroundColor;
-    this.theDiv.style.color = this.design.textColor;
-    this.theDiv.style.fontSize = this.design.fontSize + "px";
-    this.theDiv.style.fontFamily = this.design.fontFamily;
-    this.theDiv.style.padding = this.design.padding + "px";
-    let shadow = "0px 0px " + this.design.shadowBlur + "px ";
-    shadow += this.design.shadowWidth + "px ";
-    shadow += "rgba(0,0,0," + this.design.shadowAlpha + ")";
+    this.theDiv.style.zIndex = design.zIndex;
+    this.theDiv.style.backgroundColor = design.backgroundColor;
+    this.theDiv.style.color = design.textColor;
+    this.theDiv.style.fontSize = design.fontSize + "px";
+    this.theDiv.style.fontFamily = design.fontFamily;
+    this.theDiv.style.border = design.border;
+    this.theDiv.style.borderWidth = design.borderWidth + "px";
+    this.theDiv.style.borderColor = design.borderColor;
+    this.theDiv.style.padding = design.padding + "px";
+    let shadow = "0px 0px " + design.shadowBlur + "px ";
+    shadow += design.shadowWidth + "px ";
+    shadow += "rgba(" + design.shadowRed + "," + design.shadowGreen + "," + design.shadowBlue + "," + design.shadowAlpha + ")";
     this.theDiv.style.boxShadow = shadow;
-    this.theDiv.style.borderRadius = this.design.borderRadius + "px";
+    this.theDiv.style.borderRadius = design.borderRadius + "px";
     this.resize();
 };
 
