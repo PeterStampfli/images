ccdf518d7e7fd3b1c5f2837c5230214c5f679cc3
PeterStampfli <stampfli@macquality.ch>
2020-02-24 16:33:02 +0100

    output div moved to paramGui.js, dimension problem corrected

--- libgui/paramGui.js
+++ libgui/paramGui.js
@@ -805,7 +805,6 @@ ParamGui.createArgs = function(theParams, theProperty, low, high, step) {
     return args;
 };
 
-
 /**
  * add a controller for a parameter, one controller on a line, in its div
  * depending on its value and limits
@@ -1060,4 +1059,124 @@ ParamGui.prototype.closePopup = function() {
             element.closePopup();
         }
     });
+};
+
+// output elements
+//  create a div container for output elements (canvas)
+//==============================================================
+
+/**
+ * get the limit for free space at the left
+ * taking into account all guis at the left side
+ * @method ParamGui#leftSpaceLimit
+ * @return number, for the left offset of a fitting container
+ */
+ParamGui.leftSpaceLimit = function() {
+    let spaceLimit = 0;
+    ParamGui.rootGuis.forEach(gui => {
+        if (gui.design.horizontalPosition === "left") {
+            spaceLimit = Math.max(gui.design.horizontalShift + gui.design.width + 2 * gui.design.borderWidth, spaceLimit);
+        }
+    });
+    return spaceLimit;
+};
+
+/**
+ * get the limit for free space at the right
+ * taking into account all guis at the right hand side
+ * @method ParamGui#rightSpaceLimit
+ * @return number, the width of fitting container is difference between right and left limit
+ */
+ParamGui.rightSpaceLimit = function() {
+    let space = 0;
+    ParamGui.rootGuis.forEach(gui => {
+        if (gui.design.horizontalPosition === "right") {
+            space = Math.max(gui.design.horizontalShift + gui.design.width + 2 * gui.design.borderWidth, space);
+        }
+    });
+    // with window.innerWidth we do not get some ghosts of scroll bars
+    // document.documentElement.clientWidth upon reducing screen height gives a smaller width because of spurious vertical scroll bar
+  //  const spaceLimit = window.innerWidth - space;
+    const spaceLimit = document.documentElement.clientWidth - space;
+    return spaceLimit;
+};
+
+ParamGui.outputDiv = false;
+
+/**
+* resizing the content of the output div
+* does nothing here, overwrite depending on content
+* output div will have its final dimension when calling this
+* so you can use its clientWidth/Height
+* @method ParamGui.resizeOutputContent
+*/
+ParamGui.resizeOutputContent=function(){};
+
+/**
+ * resizing the output div and setting its scrolling
+ * sets width and height of the output div
+ * calls resize function for contents
+ * determines size of content
+ * then activates scroll bars
+ * @method ParamGui.resizeOutputDiv
+ */
+ParamGui.resizeOutputDiv = function() {
+    // fit height to window, avoid spurious vertical scroll bar in calculation of horizontal space
+    ParamGui.outputDiv.style.height = window.innerHeight + "px";
+    const leftOfSpace = ParamGui.leftSpaceLimit();
+    const widthOfSpace = ParamGui.rightSpaceLimit() - leftOfSpace;
+    ParamGui.outputDiv.style.left = leftOfSpace + "px";
+    // resize content: set up dimensions of the div
+    // you can use them to resize content
+    // overflow hidden makes that scroll bars do not appear
+    ParamGui.outputDiv.style.width = widthOfSpace + "px";
+    ParamGui.outputDiv.style.overflow = "hidden";
+    ParamGui.resizeOutputContent();
+    // get size of contents
+    // no height and width given => shrink wrap
+    ParamGui.outputDiv.style.height = "";
+    ParamGui.outputDiv.style.width = "";
+    // clientWidth/Height give dimensions of content
+    const widthOfContent = ParamGui.outputDiv.clientWidth;
+    const heightOfContent = ParamGui.outputDiv.clientHeight;
+    // set final dimensions
+    ParamGui.outputDiv.style.width = widthOfSpace + "px";
+        ParamGui.outputDiv.style.height = window.innerHeight + "px";
+    // see if content is too wide and horizontal scroll bars are required
+    if (widthOfContent > ParamGui.outputDiv.clientWidth) {
+        ParamGui.outputDiv.style.overflowX = "scroll";
+    }
+    // see if content is too high (including a possible scroll bar)
+    if (heightOfContent > ParamGui.outputDiv.clientHeight) {
+        ParamGui.outputDiv.style.overflowY = "scroll";
+        // the clientwidth has been reduced, and we may need a herizontal scroll bar now
+        if (widthOfContent > ParamGui.outputDiv.clientWidth) {
+            ParamGui.outputDiv.style.overflowX = "scroll";
+        }
+    }
+};
+
+/**
+ * create an output div fitting the guis
+ * it will be in ParamGui.outputDiv
+ * it will fit between the guis at the left and those at the right
+ * you can have more than one canvas in the output div ...
+ * @method ParamGui.createOutputDiv
+ * @return the output div
+ */
+ParamGui.createOutputDiv = function() {
+    if (ParamGui.outputDiv) {
+        console.error("ParamGui.outputDiv exists already!");
+    } else {
+        ParamGui.outputDiv = document.createElement("div");
+        ParamGui.outputDiv.style.position = "absolute";
+        ParamGui.outputDiv.style.top = "0px";
+
+        ParamGui.outputDiv.style.backgroundColor = "blue";
+
+        ParamGui.resizeOutputDiv();
+        document.body.appendChild(ParamGui.outputDiv);
+        window.addEventListener("resize", ParamGui.resizeOutputDiv, false);
+    }
+    return ParamGui.outputDiv;
 };
