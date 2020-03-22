543e75df36f091e471e0fcd49ecd2af77ee740c1
PeterStampfli <pestampf@gmail.com>
2020-03-22 16:06:24 +0100

    progressing life draw linear interpolation, cubic and quantization

--- gameOfSymmetry/lifeShowSquareRGB.js
+++ gameOfSymmetry/lifeShowSquareRGB.js
@@ -41,6 +41,8 @@ var red, green, blue;
 var uiController;
 var nStates = 0;
 var imageSize = 0;
+var quantFactor = 1; // factor for quantization of interpolated values
+var quantize = false; // after interpolation
 
 /**
  *
@@ -58,7 +60,8 @@ lifeShowSquareRGB.init = function(theGui, theCanvas) {
     // set up the gui
     const choices = {
         'greyBlockPixels': greyBlock,
-        'greyLinearInterpolation': greyLinear,
+        'greyLinearInterpolation': greyLinearContinuous,
+        'greyLinearInterpolationQuantized': greyLinearQuantized,
         'greyCubicInterpolation': greyCubic,
         'colorBlockPixels': colorBlock,
         'colorLinearInterpolation': colorLinear,
@@ -68,7 +71,7 @@ lifeShowSquareRGB.init = function(theGui, theCanvas) {
         type: 'selection',
         options: choices,
         params: lifeShowSquareRGB,
-        property: 'draw',
+        property: 'drawMethod',
         labelText: 'display',
         onChange: function() {
             lifeShowSquareRGB.draw();
@@ -100,14 +103,20 @@ lifeShowSquareRGB.showUI = function() {
  */
 lifeShowSquareRGB.setNumberOfStates = function(n) {
     nStates = Math.max(2, n);
+    quantFactor = (n - 0.00001) / (n - 1); // stretching the intevall [0,n-1] to [0,n), for giving equal weight to each possible state (rounding down)
     clearImage();
 };
+
+// the method for drawing
+lifeShowSquareRGB.drawMethod = greyBlock;
 
 /**
  * (re)draws the image after changing the canvas size or display method
  * @method lifeShowSquareRGB.draw
  */
-lifeShowSquareRGB.draw = greyBlock;
+lifeShowSquareRGB.draw = function() {
+    lifeShowSquareRGB.drawMethod();
+};
 
 /**
  * read data of a (new) generation of the world as an array
@@ -123,9 +132,9 @@ lifeShowSquareRGB.inputGeneration = function(worldData) {
     // if size changes we update the image size and data arrays
     if (worldSize !== imageSize + 2) {
         imageSize = worldSize - 2;
-        red.length = imageSize*imageSize;
-        green.length = imageSize*imageSize;
-        blue.length = imageSize*imageSize;
+        red.length = imageSize * imageSize;
+        green.length = imageSize * imageSize;
+        blue.length = imageSize * imageSize;
         clearImage();
     }
     // generations go from green to red to blue: exchange data arrays
@@ -142,7 +151,7 @@ lifeShowSquareRGB.inputGeneration = function(worldData) {
         for (var i = 1; i <= imageSize; i++) {
             worldIndex += 1;
             imageIndex += 1;
-            green[imageIndex] = Math.max(0,Math.min(255, Math.floor(factor * worldData[worldIndex])));
+            green[imageIndex] = worldData[worldIndex];
         }
     }
     lifeShowSquareRGB.draw();
@@ -160,21 +169,22 @@ function clearImage() {
 function greyBlock() {
     console.log('drawing grey blocks');
     const canvasSize = Math.min(canvas.height, canvas.width);
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height); 
-    const pixelSize = Math.floor(canvasSize / imageSize);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    const pixelSize = canvasSize / imageSize;
+    const factor = 255.9 / (nStates - 1);
     var i, j;
     let y = 0;
     let index = 0;
     for (j = 0; j < imageSize; j++) {
         let x = 0;
         for (i = 0; i < imageSize; i++) {
-        	// grey level as hex string with two digits
-        	// note that '#aaa' is equivalent to '#aaaaaa' and not '#0a0a0a'
-        	let grey=green[index].toString(16);
-        	if (grey.length<2){
-        		grey='0'+grey;
-        	}
-            canvasContext.fillStyle = '#'+grey+grey+grey;
+            // grey level as hex string with two digits
+            // note that '#aaa' is equivalent to '#aaaaaa' and not '#0a0a0a'
+            let grey = Math.floor(factor * green[index]).toString(16);
+            if (grey.length < 2) {
+                grey = '0' + grey;
+            }
+            canvasContext.fillStyle = '#' + grey + grey + grey;
             canvasContext.fillRect(x, y, pixelSize, pixelSize);
             x += pixelSize;
             index += 1;
@@ -185,30 +195,31 @@ function greyBlock() {
 
 function colorBlock() {
     console.log('drawing color blocks');
-       const canvasSize = Math.min(canvas.height, canvas.width);
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height); 
-    const pixelSize = Math.floor(canvasSize / imageSize);
+    const canvasSize = Math.min(canvas.height, canvas.width);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    const pixelSize = canvasSize / imageSize;
+    const factor = 255.9 / (nStates - 1);
     var i, j;
     let y = 0;
     let index = 0;
     for (j = 0; j < imageSize; j++) {
         let x = 0;
         for (i = 0; i < imageSize; i++) {
-        	// grey level as hex string with two digits
-        	// note that '#aaa' is equivalent to '#aaaaaa' and not '#0a0a0a'
-        	let g=green[index].toString(16);
-        	if (g.length<2){
-        		g='0'+g;
-        	}
-        	let r=red[index].toString(16);
-        	if (r.length<2){
-        		r='0'+r;
-        	}
-        	let b=blue[index].toString(16);
-        	if (b.length<2){
-        		b='0'+b;
-        	}
-            canvasContext.fillStyle = '#'+r+g+b;
+            // grey level as hex string with two digits
+            // note that '#aaa' is equivalent to '#aaaaaa' and not '#0a0a0a'
+            let g = Math.floor(factor * green[index]).toString(16);
+            if (g.length < 2) {
+                g = '0' + g;
+            }
+            let r = Math.floor(factor * red[index]).toString(16);
+            if (r.length < 2) {
+                r = '0' + r;
+            }
+            let b = Math.floor(factor * blue[index]).toString(16);
+            if (b.length < 2) {
+                b = '0' + b;
+            }
+            canvasContext.fillStyle = '#' + r + g + b;
             canvasContext.fillRect(x, y, pixelSize, pixelSize);
             x += pixelSize;
             index += 1;
@@ -219,14 +230,174 @@ function colorBlock() {
 
 function greyLinear() {
     console.log('drawing grey linear interpolation');
+    const canvasSize = Math.min(canvas.height, canvas.width);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    const d = imageSize / canvasSize; // from pixel indices to image coordinates
+    const imageData = canvasContext.getImageData(0, 0, canvasSize, canvasSize);
+    const pixels = imageData.data;
+    const factor = 255.9 / (nStates - 1);
+    // interpolation:
+    // x and y are image coordinates
+    // in one direction only: cell 0 has center at 0.5, cell 1 at 1.5, thus
+    // x (or y) between 0.5/size and 1.5/size interpolates between cells 0 and 1, and so on
+    // x between imageSize-1.5 and imageSize-0.5 interpolates between cells imageSize-2 and imageSize-1
+    // use periodic boundary condition
+    // x close to imageSize: interpolation between cell imageSize-1 and cell 0==imageSize
+    let pixelIndex = 0;
+    let y = 0;
+    for (var jCanvas = 0; jCanvas < canvasSize; jCanvas++) {
+        let x = 0;
+        let jHigh = Math.round(y); // always 0 <= jHigh <= imageSize
+        let jLow = jHigh - 1; // always -1 <=jLow <= imageSize-1
+        const dy = y - jLow - 0.5; // element with index 0 lies at 0.5 (scaled), shift of cell centers
+        if (jHigh === imageSize) {
+            jHigh = 0; // periodic
+        }
+        if (jLow < 0) {
+            jLow = imageSize - 1; // periodic
+        }
+        // multply with row length to get column indices
+        jHigh *= imageSize;
+        jLow *= imageSize;
+        for (var iCanvas = 0; iCanvas < canvasSize; iCanvas++) {
+            // essentially the same as jHigh and jLow
+            let iHigh = Math.round(x);
+            let iLow = iHigh - 1;
+            const dx = x - iLow - 0.5; // element with index 0 lies at 0.5 (scaled), shift of cell centers
+            if (iHigh === imageSize) {
+                iHigh = 0;
+            }
+            if (iLow < 0) {
+                iLow = imageSize - 1;
+            }
+            let grey = (1 - dx) * ((1 - dy) * green[iLow + jLow] + dy * green[iLow + jHigh]);
+            grey += dx * ((1 - dy) * green[iHigh + jLow] + dy * green[iHigh + jHigh]);
+            if (quantize) {
+                grey = Math.floor(quantFactor * grey);
+            }
+            grey = Math.floor(factor * grey);
+            pixels[pixelIndex] = grey;
+            pixels[pixelIndex + 1] = grey;
+            pixels[pixelIndex + 2] = grey;
+            pixels[pixelIndex + 3] = 255;
+            pixelIndex += 4;
+            x += d;
+        }
+        y += d;
+    }
+    canvasContext.putImageData(imageData, 0, 0);
+}
+
+function greyLinearContinuous() {
+    quantize = false;
+    greyLinear();
+}
+
+function greyLinearQuantized() {
+    quantize = true;
+    greyLinear();
 }
 
 function colorLinear() {
     console.log('drawing color linear interpolation');
 }
 
+
+/*
+the interpolation kernel: linear interpolation of the kernel is much slower, the arrow function form is slightly slower
+it is normalized to 1 within an error of about 1.00001 ! (good enough)
+*/
+function kernel(x) { // Mitchell-Netrovali, B=C=0.333333, 0<x<2
+    if (x < 1) {
+        return (1.16666 * x - 2) * x * x + 0.888888;
+    }
+    return ((2 - 0.388888 * x) * x - 3.33333) * x + 1.777777;
+}
+
 function greyCubic() {
     console.log('drawing grey cubic interpolation');
+    const canvasSize = Math.min(canvas.height, canvas.width);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    const d = imageSize / canvasSize; // from pixel indices to image coordinates
+    const imageData = canvasContext.getImageData(0, 0, canvasSize, canvasSize);
+    const pixels = imageData.data;
+    const factor = 255.9 / (nStates - 1);
+    // interpolation:
+    // x and y are image coordinates
+    // in one direction only: cell 0 has center at 0.5, cell 1 at 1.5, 
+    // use periodic boundary condition
+    let pixelIndex = 0;
+    let y = 0;
+    for (var jCanvas = 0; jCanvas < canvasSize; jCanvas++) {
+        let x = 0;
+        let jHigh = Math.round(y); // always 0 <= jHigh <= imageSize
+        let jLow = jHigh - 1; // always -1 <=jLow <= imageSize-1
+        let jLower = jHigh - 2; // between -2 and imageSize-2
+        let jHigher = jHigh + 1; // between 1 and imageSize+1
+        const dy = y - jLow - 0.5; // element with index 0 lies at 0.5 (scaled), shift of cell centers
+        const weightYLow = kernel(dy);
+        const weightYHigh = kernel(1 - dy);
+        const weightYLower = kernel(1 + dy);
+        const weightYHigher = kernel(2 - dy);
+        // wraparound (periodicity)
+        if (jHigher >= imageSize) {
+            jHigher -= imageSize; // periodic
+        }
+        if (jHigh === imageSize) {
+            jHigh = 0; // periodic
+        }
+        if (jLow < 0) {
+            jLow = imageSize - 1; // periodic
+        }
+        if (jLower < 0) {
+            jLower += imageSize; // periodic
+        }
+        // multply with row length to get column indices
+        jHigher *= imageSize;
+        jHigh *= imageSize;
+        jLow *= imageSize;
+        jLower *= imageSize;
+        for (var iCanvas = 0; iCanvas < canvasSize; iCanvas++) {
+            // essentially the same as jHigh and jLow
+            let iHigh = Math.round(x);
+            let iLow = iHigh - 1;
+            let iLower = iHigh - 2;
+            let iHigher = iHigh + 1;
+            const dx = x - iLow - 0.5; // element with index 0 lies at 0.5 (scaled), shift of cell centers
+            // dx, dy relate to the cell at (xLow+yLow)
+            const weightXLow = kernel(dx);
+            const weightXHigh = kernel(1 - dx);
+            const weightXLower = kernel(1 + dx);
+            const weightXHigher = kernel(2 - dx);
+            if (iHigher >= imageSize) {
+                iHigher -= imageSize;
+            }
+            if (iHigh === imageSize) {
+                iHigh = 0;
+            }
+            if (iLow < 0) {
+                iLow = imageSize - 1;
+            }
+            if (iLower < 0) {
+                iLower += imageSize;
+            }
+            let grey = weightXLower * (weightYLower * green[iLower + jLower] + weightYLow * green[iLower + jLow] + weightYHigh * green[iLower + jHigh] + weightYHigher * green[iLower + jHigher]);
+            grey += weightXLow * (weightYLower * green[iLow + jLower] + weightYLow * green[iLow + jLow] + weightYHigh * green[iLow + jHigh] + weightYHigher * green[iLow + jHigher]);
+            grey += weightXHigh * (weightYLower * green[iHigh + jLower] + weightYLow * green[iHigh + jLow] + weightYHigh * green[iHigh + jHigh] + weightYHigher * green[iHigh + jHigher]);
+            grey += weightXHigher * (weightYLower * green[iHigher + jLower] + weightYLow * green[iHigher + jLow] + weightYHigh * green[iHigher + jHigh] + weightYHigher * green[iHigher + jHigher]);
+            grey = Math.max(0, Math.min(nStates - 1, grey)); // beware of negative values
+            grey = Math.floor(grey * (nStates - 0.00001) / (nStates - 1));
+            grey = Math.floor(factor * grey);
+            pixels[pixelIndex] = grey;
+            pixels[pixelIndex + 1] = grey;
+            pixels[pixelIndex + 2] = grey;
+            pixels[pixelIndex + 3] = 255;
+            pixelIndex += 4;
+            x += d;
+        }
+        y += d;
+    }
+    canvasContext.putImageData(imageData, 0, 0);
 }
 
 function colorCubic() {
