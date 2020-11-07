6efad2bcc1ac5258ada74a82fa434a6db0b3ab5b
PeterStampfli <pestampf@gmail.com>
2020-11-07 15:55:46 +0100

    view 3 cirles

--- kaleidoscope/view.js
+++ kaleidoscope/view.js
@@ -41,7 +41,7 @@ view.makeGui = function(parentGui, args = {}) {
         type: 'selection',
         params: view,
         property: 'type',
-        options: ['direct', 'inversion'],
+        options: ['direct', 'inversion at intersection', 'inversion at boundary'],
         onChange: function() {
             if (view.type === 'direct') {
                 view.visibleButton.hide();
@@ -56,7 +56,7 @@ view.makeGui = function(parentGui, args = {}) {
                 view.circlesMessage.style.display = 'block';
                 view.centerMessage.style.display = 'block';
             }
-            if (view.type === 'inversion') {
+            if (view.type === 'inversion at intersection') {
                 view.radiusController.show();
                 view.switchCenterButton.show();
             } else {
@@ -68,7 +68,7 @@ view.makeGui = function(parentGui, args = {}) {
     });
     let selectionHelp = 'Choose between different views:<br>';
     selectionHelp += '<strong>direct:</strong> Does not transform the kaleiddoscopic image.<br>';
-    selectionHelp += '<strong>inversion:</strong> Inverts at the intersection of two circles.<br>';
+    selectionHelp += '<strong>inversion at intersection:</strong> Inversion at a circle. Its center lies at the intersection of two circles.<br>';
     view.selectionButton.addHelp(selectionHelp);
     view.circle1 = null;
     view.circle2 = null;
@@ -192,6 +192,62 @@ view.makeCenterMessage = function() {
 };
 
 /**
+ * determine radius and center of view for three circcles
+ * either boundary of Poincare disc or equator of sphere (spherical geometry)
+ * euclidean geometry: all circles intersect at center (hyperbolic geometry: max 2 circles pass through center if they have infinite radius)
+ * sets view.isActive=false for fail
+ * @method view.updateCenterRadius
+ */
+view.updateCenterRadius = function() {
+    if (guiUtils.isObject(view.circle1) && guiUtils.isObject(view.circle2) && guiUtils.isObject(view.circle3)) {
+        const center1X = view.circle1.centerX;
+        const center1Y = view.circle1.centerY;
+        const delta1 = center1X * center1X + center1Y * center1Y - view.circle1.radius2;
+        const center2X = view.circle2.centerX;
+        const center2Y = view.circle2.centerY;
+        const delta2 = center2X * center2X + center2Y * center2Y - view.circle2.radius2;
+        const center3X = view.circle3.centerX;
+        const center3Y = view.circle3.centerY;
+        const delta3 = center3X * center3X + center3Y * center3Y - view.circle3.radius2;
+        const center1To2X = center2X - center1X;
+        const center1To2Y = center2Y - center1Y;
+        const center1To3X = center3X - center1X;
+        const center1To3Y = center3Y - center1Y;
+        // the system of linear equations for the center of this circle
+        const denom = center1To2X * center1To3Y - center1To3X * center1To2Y;
+        if (Math.abs(denom) < 0.001 * (Math.abs(center1To2X * center1To3Y) + Math.abs(center1To3X * center1To2Y))) {
+            // nearly colinear, fail
+            centerMessage = 'Fail because the circle centers are colinear.';
+            view.isActive = false;
+        } else {
+            view.centerX = 0.5 * ((delta2 - delta1) * center1To3Y - (delta3 - delta1) * center1To2Y) / denom;
+            view.centerY = 0.5 * ((delta3 - delta1) * center1To2X - (delta2 - delta1) * center1To3X) / denom;
+            // use smallest circle for precision
+            let r2 = 0;
+            if ((view.circle1 < view.circle2) && (view.circle1 < view.circle3)) {
+                r2 = (view.centerX - center1X) * (view.centerX - center1X) + (view.centerY - center1Y) * (view.centerY - center1Y) - view.circle1.radius2;
+
+            } else if ((view.circle2 < view.circle3)) {
+                r2 = (view.centerX - center2X) * (view.centerX - center2X) + (view.centerY - center2Y) * (view.centerY - center2Y) - view.circle2.radius2;
+            } else {
+                r2 = (view.centerX - center3X) * (view.centerX - center3X) + (view.centerY - center3Y) * (view.centerY - center3Y) - view.circle3.radius2;
+            }
+            if (r2 > 0.00001) {
+                view.radius = Math.sqrt(r2);
+                view.isActive = true;
+            } else {
+                centerMessage = 'Fail because the circles intersect at a single point (Euclidean geometry).';
+
+                view.isActive = false;
+            }
+        }
+    } else {
+        centerMessage = 'This view requires three intersecting circles.';
+        view.isActive = false;
+    }
+};
+
+/**
  * update the view:
  * position of center and radius, message, transform, 
  * always call when map changes
@@ -205,7 +261,7 @@ view.update = function() {
         case 'direct':
             view.isActive = false;
             break;
-        case 'inversion':
+        case 'inversion at intersection':
             if (guiUtils.isObject(view.circle1) && guiUtils.isObject(view.circle2)) {
                 if (view.circle1.intersectsCircle(view.circle2)) {
                     view.circle1.determineIntersectionPositions(pos1, pos2, view.circle2);
