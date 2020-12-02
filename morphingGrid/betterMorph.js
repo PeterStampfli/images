8660244b3625323eb5e30c4fba48ae1bd53a7873
PeterStampfli <pestampf@gmail.com>
2020-12-02 21:04:15 +0100

    better morphing grid

--- morphingGrid/betterMorph.js
+++ morphingGrid/betterMorph.js
@@ -1,29 +1,18 @@
 /* jshint esversion: 6 */
 
 import {
-    CoordinateTransform,
     ParamGui,
     Pixels,
-    guiUtils,
     output
+
 } from "../libgui/modules.js";
 
 export const morph = {};
 
-morph.nPeriods = 4;
-
-morph.x=0.5;
-
-// drawing
 morph.draw = function() {
+    output.isDrawing = true;
     output.pixels.update();
-    const canvas = output.canvas;
-    const height = canvas.height;
-    const width = canvas.width;
-    const scale = 2 * Math.PI * morph.nPeriods / width;
-    const pi2 = Math.PI / 2;
-    const mix=1-Math.cos(morph.x*pi2);
-    console.log(mix,morph.pi);
+    const pixels = output.pixels;
     const white = Pixels.integerOfColor({
         red: 255,
         green: 255,
@@ -36,31 +25,30 @@ morph.draw = function() {
         blue: 0,
         alpha: 255
     });
-    const width2 = width / 2;
-    const height2 = height / 2;
     const rt2 = Math.sqrt(0.5);
+    const mapWidth = (output.canvas.width - 1) * pixels.antialiasSubpixels + pixels.antialiasSampling;
+    const mapHeight = (output.canvas.height - 1) * pixels.antialiasSubpixels + pixels.antialiasSampling;
+    let scale = output.coordinateTransform.totalScale / output.pixels.antialiasSubpixels;
+    let shiftX = output.coordinateTransform.shiftX;
+    let shiftY = output.coordinateTransform.shiftY;
     let index = 0;
-    var x, y;
-    for (var j = 0; j < height; j++) {
-        y = scale * (j - height2) - pi2;
-        for (var i = 0; i < width; i++) {
-            x = scale * (i - width2) - pi2;
-            let wave1 = Math.cos(x) * Math.cos(y);
-            let wave2 = Math.cos((x + y) * rt2) * Math.cos((x - y) * rt2);
-            let wave = mix * wave2 + (1 - mix) * wave1;
+    let y = shiftY;
+    for (var j = 0; j < mapHeight; j++) {
+        let x = shiftX;
+        for (var i = 0; i < mapWidth; i++) {
+            let wave = Math.cos(x) * Math.cos(y);
             if (wave > 0) {
                 output.pixels.array[index] = white;
             } else {
                 output.pixels.array[index] = black;
             }
             index += 1;
+            x += scale;
         }
+        y += scale;
     }
     output.pixels.show();
 };
-
-// setup parameters
-const canvasSize=512;
 
 morph.setup = function() {
     // base gui
@@ -70,12 +58,13 @@ morph.setup = function() {
     });
     morph.gui = gui;
     // create an output canvas
-    output.createCanvas(gui,false,false);
- //   output.setCanvasWidthToHeight();
-    output.canvasWidthController.setValue(canvasSize);
+    output.createCanvas(gui, false, false);
+    output.setCanvasWidthToHeight();
     output.createPixels();
+    output.addImageProcessing();
     output.addAntialiasing();
+    output.addCoordinateTransform(false);
+    output.setInitialCoordinates(-3.5 * Math.PI, -3.5 * Math.PI, 8 * Math.PI);
     output.drawCanvasChanged = morph.draw;
-    output.startDrawing();
     morph.draw();
 };
