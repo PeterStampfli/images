9b2badd40adb086f20338073afb276782e5432a4
PeterStampfli <pestampf@gmail.com>
2020-12-25 14:50:05 +0100

    basic shapes for one+sqrt(3)

--- randomTlings/oneSqrt3.js
+++ randomTlings/oneSqrt3.js
@@ -6,21 +6,6 @@ import {
 }
 from "../libgui/modules.js";
 
-
-
-// overprinting to avoid gaps
-// center for overprinting
-let overCenterX = 0;
-let overCenterY = 0;
-// overprinting factor is truchet.overprint
-
-
-
-/**
- * linear interpolation for numbers
- */
-
-
 const gui = new ParamGui({
     closed: false
 });
@@ -91,13 +76,6 @@ gui.add(colorController, {
 const rt32 = 0.5 * Math.sqrt(3);
 const rt3 = Math.sqrt(3);
 
-function line(x1, y1, x2, y2) {
-    canvasContext.beginPath();
-    canvasContext.moveTo(x1, y1);
-    canvasContext.lineTo(x2, y2);
-    canvasContext.stroke();
-}
-
 // actually the minimum square, center of full square at (blX,blY), (trX,trY) is opposite corner
 function square(gen, blX, blY, trX, trY) {
     // make center and missing corners
@@ -109,22 +87,49 @@ function square(gen, blX, blY, trX, trY) {
     const brY = cY - dX;
     const tlX = cX - dY;
     const tlY = cY + dY;
-    output.makePath(blX, blY, brX, brY, trX, trY, tlX, tlY);
-    output.canvasContext.stroke();
     if (gen >= truchet.maxGen) {
         canvasContext.fillStyle = truchet.square;
+        output.makePath(blX, blY, brX, brY, trX, trY, tlX, tlY);
+        canvasContext.fill();
+        canvasContext.strokeStyle = truchet.lineColor;
+        output.makePath(brX, brY, trX, trY, tlX, tlY);
+        canvasContext.stroke();
 
     } else {
 
         // substitution: determine "right" and "up" directions. Vectorlength=side length of new tiles
-        const f = 2 / (1 + rt3);
-        const upX = f * (tlX - blX);
+        // 0.732050808 = 2 / (1 + rt3);
+        const upX = 0.732050808 * (tlX - blX);
     }
 }
 
 // the rhomb, coordinates of the corners with acute angles
 function rhomb(gen, bX, bY, tX, tY) {
+    // make center and missing corners
+    const cX = 0.5 * (bX + tX);
+    const cY = 0.5 * (bY + tY);
+    // 0.378937382=tan(Math.PI/12)*sqrt(2);
+    const upX = 0.378937382 * (cX - bX);
+    const upY = 0.378937382 * (cY - bY);
+    const rightX = upY;
+    const rightY = -upX;
+    const rX = cX + 0.7071 * rightX;
+    const rY = cY - 0.7071 * rightY;
 
+    const lX = cX - 0.7071 * rightX;
+    const lY = cY + 0.7071 * rightY;
+    if (gen >= truchet.maxGen) {
+        canvasContext.fillStyle = truchet.rhomb;
+        canvasContext.strokeStyle = truchet.lineColor;
+        output.makePath(bX, bY, rX, rY, tX, tY, lX, lY);
+        canvasContext.closePath();
+        canvasContext.fill();
+        canvasContext.stroke();
+
+    } else {
+
+
+    }
 }
 
 // halves of triangles, asymmetric!
@@ -132,6 +137,20 @@ function rhomb(gen, bX, bY, tX, tY) {
 // (bX,bY) is other point at base. 60 degree angle corner
 // (cX,cY) is top, 30 degree angle corner
 function triangleA(gen, mX, mY, bX, bY, cX, cY) {
+    if (gen >= truchet.maxGen) {
+        canvasContext.fillStyle = truchet.triangle;
+        canvasContext.strokeStyle = truchet.lineColor;
+        output.makePath(mX, mY, bX, bY, cX, cY);
+        canvasContext.closePath();
+        canvasContext.fill();
+        canvasContext.stroke();
+
+    } else {
+        // make directions
+        // 0.732050808 = 2 / (1 + rt3);
+
+    }
+
 
 }
 
@@ -140,7 +159,9 @@ function draw() {
     output.fillCanvasBackgroundColor();
     output.correctYAxis();
 
-    square(1, 0, 0, 100, 100);
+    //  square(1, 0, 0, 100, 100);
+    //   rhomb(1, 0, 0, 0, 100);
+    triangleA(1, 0, 0, 100, 0, 0, 100 * rt3);
     output.drawGrid();
 }
 
