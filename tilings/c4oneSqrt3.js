0790db22fabf44460a189b102b676fe145b09918
PeterStampfli <pestampf@gmail.com>
2021-03-04 21:16:37 +0100

    c4 1+sqrt(3)

---
+++ tilings/c4oneSqrt3.js
@@ -0,0 +1,523 @@
+/* jshint esversion: 6 */
+
+import {
+    ParamGui,
+    output,
+    BooleanButton
+}
+from "../libgui/modules.js";
+
+const gui = new ParamGui({
+    closed: false
+});
+
+output.createCanvas(gui, {
+    name: 'canvas control',
+});
+const canvas = output.canvas;
+const canvasContext = canvas.getContext('2d');
+
+output.addCoordinateTransform();
+output.addCursorposition();
+output.setInitialCoordinates(0, 0, 210);
+output.addGrid();
+
+// parameters for drawing
+const tiling = {};
+// colors
+tiling.rhomb30Color = '#ff0000';
+tiling.squareColor = '#00ff00';
+tiling.triangleRColor = '#0000ff';
+tiling.triangleLColor = '#00ffff';
+tiling.marker = true;
+tiling.markerSize = 1;
+tiling.markerColor = '#888888ff';
+tiling.borderColor = '#000000';
+tiling.borderWidth = 2;
+tiling.border = true;
+tiling.hyperBorderColor = '#000000';
+tiling.hyperBorderWidth = 3;
+tiling.hyperBorder = false;
+tiling.decoration = 'solid color';
+tiling.gridWidth = 3;
+tiling.gridColor = '#ff8800';
+
+tiling.maxGen = 1;
+tiling.initial = 'quarter square';
+
+const colorController = {
+    type: 'color',
+    params: tiling,
+    onChange: function() {
+        draw();
+    }
+};
+
+const widthController = {
+    type: 'number',
+    params: tiling,
+    min: 0.1,
+    onChange: function() {
+        draw();
+    }
+};
+
+gui.addParagraph('<strong>appearance of tiles</strong>');
+
+gui.add({
+    type: 'selection',
+    params: tiling,
+    property: 'decoration',
+    options: ['none', 'solid color', 'grid'],
+    onChange: function() {
+        if (tiling.decoration === 'grid') {
+            gridWidthController.show();
+            gridColorController.show();
+        } else {
+            gridWidthController.hide();
+            gridColorController.hide();
+        }
+        if (tiling.decoration === 'solid color') {
+            rhomb30ColorController.show();
+            squareColorController.show();
+            triangleRColorController.show();
+            triangleLColorController.show();
+        } else {
+            rhomb30ColorController.hide();
+            squareColorController.hide();
+            triangleRColorController.hide();
+            triangleLColorController.hide();
+        }
+        draw();
+    }
+});
+
+const gridColorController = gui.add(colorController, {
+    property: 'gridColor',
+    labelText: 'grid'
+});
+
+const gridWidthController = gui.add(widthController, {
+    property: 'gridWidth',
+    labelText: 'width of grid'
+});
+gridWidthController.hide();
+gridColorController.hide();
+
+const rhomb30ColorController = gui.add(colorController, {
+    property: 'rhomb30Color',
+    labelText: 'rhomb30'
+});
+
+const squareColorController = gui.add(colorController, {
+    property: 'squareColor',
+    labelText: 'square'
+});
+
+const triangleRColorController = gui.add(colorController, {
+    property: 'triangleRColor',
+    labelText: 'triangleR'
+});
+
+const triangleLColorController = gui.add(colorController, {
+    property: 'triangleLColor',
+    labelText: 'triangleL'
+});
+
+const markerColorController = gui.add(colorController, {
+    property: 'markerColor',
+    labelText: 'marker'
+});
+markerColorController.hide();
+
+BooleanButton.greenRedBackground();
+const borderController = gui.add({
+    type: 'boolean',
+    params: tiling,
+    property: 'border',
+    onChange: function() {
+        draw();
+    }
+});
+
+borderController.add(widthController, {
+    property: 'borderWidth',
+    labelText: 'width'
+});
+
+gui.add(colorController, {
+    property: 'borderColor',
+    labelText: 'color'
+});
+
+const hyperBorderController = gui.add({
+    type: 'boolean',
+    params: tiling,
+    property: 'hyperBorder',
+    onChange: function() {
+        draw();
+    }
+});
+
+hyperBorderController.add(widthController, {
+    property: 'hyperBorderWidth',
+    labelText: 'width'
+});
+
+gui.add(colorController, {
+    property: 'hyperBorderColor',
+    labelText: 'color'
+});
+
+const markerController = gui.add({
+    type: 'boolean',
+    params: tiling,
+    property: 'marker',
+    onChange: function() {
+        draw();
+    }
+});
+
+markerController.add(widthController, {
+    property: 'markerSize',
+    labelText: 'size'
+});
+
+gui.add(colorController, {
+    property: 'markerColor',
+    labelText: 'color'
+});
+
+gui.addParagraph('<strong>tiling</strong>');
+
+gui.add({
+    type: 'selection',
+    params: tiling,
+    property: 'initial',
+    options: ['quarter square', 'rhomb30', 'triangleR', 'triangleL', 'square','dodecagon'],
+    onChange: function() {
+        draw();
+    }
+});
+
+gui.add({
+    type: 'number',
+    params: tiling,
+    property: 'maxGen',
+    labelText: 'iterations',
+    min: 0,
+    step: 1,
+    onChange: function() {
+        draw();
+    }
+});
+
+// tiles of the tiling
+//--------------------
+const rt32 = 0.5 * Math.sqrt(3);
+const rt3 = Math.sqrt(3);
+
+// the quarter square
+
+
+// actually the minimum square, center of full square at (blX,blY), (trX,trY) is opposite corner
+function quarterSquare(gen, blX, blY, trX, trY) {
+    // make center and missing corners
+    let cX = 0.5 * (blX + trX);
+    let cY = 0.5 * (blY + trY);
+    const dX = trX - cX;
+    const dY = trY - cY;
+    const brX = cX + dY;
+    const brY = cY - dX;
+    const tlX = cX - dY;
+    const tlY = cY + dX;
+    // substitution: determine "right" and "up" directions. Vectorlength=side length of new tiles
+    // 0.732050808 = 2 / (1 + rt3);
+    const upX = 0.732050808 * (tlX - blX);
+    const upY = 0.732050808 * (tlY - blY);
+    const rightX = upY;
+    const rightY = -upX;
+    if (output.isInCanvas(blX, blY, brX, brY, trX, trY, tlX, tlY)) {
+        if (gen >= tiling.maxGen) {
+
+        } else {
+            gen += 1;
+            cX = brX + 0.5 * (upX - rightX);
+            cY = brY + 0.5 * (upY - rightY);
+
+            rhomb30(gen, blX, blY, trX, trY);
+        }
+    }
+}
+
+// actually the full square (blX,blY), (trX,trY) is opposite corner
+function square(gen, blX, blY, trX, trY) {
+    // make center and missing corners
+    let cX = 0.5 * (blX + trX);
+    let cY = 0.5 * (blY + trY);
+    const dX = trX - cX;
+    const dY = trY - cY;
+    const brX = cX + dY;
+    const brY = cY - dX;
+    const tlX = cX - dY;
+    const tlY = cY + dX;
+    // substitution: determine "right" and "up" directions. Vectorlength=side length of new tiles
+    // 0.366025404 = 1 / (1 + rt3);
+    const upX = 0.366025404 * (tlX - blX);
+    const upY = 0.366025404 * (tlY - blY);
+    const rightX = upY;
+    const rightY = -upX;
+    if (output.isInCanvas(blX, blY, brX, brY, trX, trY, tlX, tlY)) {
+        if (gen >= tiling.maxGen) {
+            if (tiling.drawBorders) {
+                if (tiling.border) {
+                    canvasContext.strokeStyle = tiling.borderColor;
+                    output.setLineWidth(tiling.borderWidth);
+                    output.makePath(blX, blY, brX, brY, trX, trY, tlX, tlY);
+                    canvasContext.closePath();
+                    canvasContext.stroke();
+                }
+            } else {
+                switch (tiling.decoration) {
+                    case 'solid color':
+                        canvasContext.fillStyle = tiling.squareColor;
+                        output.makePath(blX, blY, brX, brY, trX, trY, tlX, tlY);
+                        canvasContext.fill();
+                        break;
+                    case 'grid':
+                        canvasContext.strokeStyle = tiling.gridColor;
+                        output.setLineWidth(tiling.gridWidth);
+                        output.makePath(0.5 * (blX + brX), 0.5 * (blY + brY), 0.5 * (tlX + trX), 0.5 * (tlY + trY));
+                        canvasContext.stroke();
+                        output.makePath(0.5 * (blX + tlX), 0.5 * (blY + tlY), 0.5 * (brX + trX), 0.5 * (brY + trY));
+                        canvasContext.stroke();
+                        break;
+                }
+            }
+        } else {
+            gen += 1;
+            quarterSquare(gen, cX, cY, blX, blY);
+            quarterSquare(gen, cX, cY, brX, brY);
+            quarterSquare(gen, cX, cY, tlX, tlY);
+            quarterSquare(gen, cX, cY, trX, trY);
+
+            if (tiling.drawBorders && tiling.hyperBorder && (gen === tiling.maxGen)) {
+                canvasContext.strokeStyle = tiling.hyperBorderColor;
+                output.setLineWidth(tiling.hyperBorderWidth);
+                output.makePath(blX, blY, brX, brY, trX, trY, tlX, tlY, blX, blY);
+                canvasContext.stroke();
+            }
+        }
+    }
+}
+
+// the 30 degrees rhomb, coordinates of the corners with acute angles
+function rhomb30(gen, bX, bY, tX, tY) {
+    // make center and missing corners
+    const cX = 0.5 * (bX + tX);
+    const cY = 0.5 * (bY + tY);
+    // 0.378937382=tan(Math.PI/12)*sqrt(2);
+    const upX = 0.378937382 * (cX - bX);
+    const upY = 0.378937382 * (cY - bY);
+    const rightX = upY;
+    const rightY = -upX;
+    const rX = cX + 0.7071 * rightX;
+    const rY = cY + 0.7071 * rightY;
+    const lX = cX - 0.7071 * rightX;
+    const lY = cY - 0.7071 * rightY;
+    if (output.isInCanvas(bX, bY, rX, rY, tX, tY, lX, lY)) {
+        if (gen >= tiling.maxGen) {
+            if (tiling.drawBorders) {
+                if (tiling.border) {
+                    canvasContext.strokeStyle = tiling.borderColor;
+                    output.setLineWidth(tiling.borderWidth);
+                    output.makePath(bX, bY, rX, rY, tX, tY, lX, lY);
+                    canvasContext.closePath();
+                    canvasContext.stroke();
+                }
+            } else {
+                switch (tiling.decoration) {
+                    case 'solid color':
+                        canvasContext.fillStyle = tiling.rhomb30Color;
+                        output.makePath(bX, bY, rX, rY, tX, tY, lX, lY);
+                        canvasContext.fill();
+                        break;
+                    case 'grid':
+                        canvasContext.strokeStyle = tiling.gridColor;
+                        output.setLineWidth(tiling.gridWidth);
+                        // 1.274519=(1+sqrt(3))*cos(15)-0.5*(1+sqrt(3))/cos(15)
+                        const lowX = cX - 1.2247 * upX;
+                        const lowY = cY - 1.2247 * upY;
+                        const highX = cX + 1.2247 * upX;
+                        const highY = cY + 1.2247 * upY;
+                        output.makePath(0.5 * (bX + lX), 0.5 * (bY + lY), lowX, lowY, 0.5 * (bX + rX), 0.5 * (bY + rY));
+                        canvasContext.stroke();
+                        output.makePath(0.5 * (tX + lX), 0.5 * (tY + lY), highX, highY, 0.5 * (tX + rX), 0.5 * (tY + rY));
+                        canvasContext.stroke();
+                        output.makePath(lowX, lowY, highX, highY);
+                        canvasContext.stroke();
+                        break;
+                }
+            }
+        } else {
+            gen += 1;
+            const bcX = cX - 0.7071 * upX;
+            const bcY = cY - 0.7071 * upY;
+            const tcX = cX + 0.7071 * upX;
+            const tcY = cY + 0.7071 * upY;
+            rhomb30(gen, bX, bY, bcX, bcY);
+            rhomb30(gen, tX, tY, tcX, tcY);
+            //   0.3660254=1/(1+sqrt(3))
+            //   0.6339745=sqrt(3)/(1+sqrt(3))
+            square(gen, rX, rY, lX, lY);
+
+            if (tiling.drawBorders && tiling.hyperBorder && (gen === tiling.maxGen)) {
+                canvasContext.strokeStyle = tiling.hyperBorderColor;
+                output.setLineWidth(tiling.hyperBorderWidth);
+                output.makePath(bX, bY, rX, rY, tX, tY, lX, lY);
+                canvasContext.closePath();
+                canvasContext.stroke();
+            }
+        }
+    }
+}
+
+// third of a triangle, substitution triangles at right
+// base a to b
+// a is corner with rhomb30, b has triangles, c has triangle, apex
+// abc winds right or left as seen from center of triangle
+function triangleR(gen, aX, aY, bX, bY, cX, cY) {
+    if (output.isInCanvas(aX, aY, bX, bY, cX, cY)) {
+        // make directions
+        // 0.366025404 = 1 / (1 + rt3);
+        const rightX = 0.366025404 * (bX - aX);
+        const rightY = 0.366025404 * (bY - aY);
+        const upX = -rightY;
+        const upY = rightX;
+        if (gen >= tiling.maxGen) {
+            if (tiling.marker) {
+                canvasContext.fillStyle = tiling.markerColor;
+                const s = tiling.markerSize;
+                // marker at a for left triangle
+                // right triangle has marker at b
+                output.makePath(aX, aY, aX + s * rightX, aY + s * rightY, aX + s * 0.5 * rightX + s * rt32 * upX, aY + s * 0.5 * rightY + s * rt32 * upY);
+                canvasContext.fill();
+            }
+            if (tiling.drawBorders) {
+                if (tiling.border) {
+                    canvasContext.strokeStyle = tiling.borderColor;
+                    output.setLineWidth(tiling.borderWidth);
+                    output.makePath(aX, aY, bX, bY, cX, cY);
+                    canvasContext.closePath();
+                    canvasContext.stroke();
+                }
+            } else {
+                switch (tiling.decoration) {
+                    case 'solid color':
+                        canvasContext.fillStyle = tiling.triangleRColor;
+                        output.makePath(aX, aY, bX, bY, cX, cY);
+                        canvasContext.fill();
+                        break;
+                    case 'grid':
+                        canvasContext.strokeStyle = tiling.gridColor;
+                        output.setLineWidth(tiling.gridWidth);
+                        const zX = 0.3333 * (aX + bX + cX);
+                        const zY = 0.3333 * (aY + bY + cY);
+                        // redo grid
+                        output.makePath(0.5 * (aX + bX), 0.5 * (aY + bY), zX, zY, 0.5 * (aX + cX), 0.5 * (aY + cY));
+                        canvasContext.stroke();
+                        output.makePath(0.5 * (cX + bX), 0.5 * (cY + bY), zX, zY);
+                        canvasContext.stroke();
+                        break;
+                }
+            }
+        } else {
+            gen += 1;
+
+            if (tiling.drawBorders && tiling.hyperBorder && (gen === tiling.maxGen)) {
+                canvasContext.strokeStyle = tiling.hyperBorderColor;
+                output.setLineWidth(tiling.hyperBorderWidth);
+                output.makePath(aX, aY, bX, bY, cX, cY);
+                canvasContext.closePath();
+                canvasContext.stroke();
+            }
+        }
+    }
+}
+
+
+const size = 50; //length of side
+
+const basicX = [];
+basicX.length = 15;
+
+const basicY = [];
+basicY.length = 15;
+
+for (let i = 0; i < 15; i++) {
+    basicX[i] = Math.cos(Math.PI * i / 6) * size;
+    basicY[i] = Math.sin(Math.PI * i / 6) * size;
+}
+
+const secondX = [];
+const secondY = [];
+secondX.length = 14;
+secondY.length = 14;
+
+for (let i = 0; i < 14; i++) {
+    secondX[i] = basicX[i] + basicX[i + 1];
+    secondY[i] = basicY[i] + basicY[i + 1];
+}
+
+// make that all sides have the same length, initially, for good substitution rule figures
+function tile() {
+    let s = 200;
+    if (tiling.maxGen === 0) {
+        s /= 1 + Math.sqrt(3);
+    }
+    const r = s / Math.sqrt(3);
+    const z = s * Math.cos(Math.PI / 12);
+    switch (tiling.initial) {
+        case 'quarter square':
+            quarterSquare(0, 0, 0, s / 2, s / 2);
+            break;
+        case 'square':
+            square(0, -s / 2, -s / 2, s / 2, s / 2);
+            break;
+        case 'rhomb30':
+            rhomb30(0, -z, 0, z, 0);
+            break;
+        case 'triangleR':
+            triangleR(0, -rt32 * r, -r / 2, rt32 * r, -r / 2, 0, r);
+            break;
+        case 'triangleL':
+            triangleL(0, -rt32 * r, -r / 2, 0, r, rt32 * r, -r / 2);
+            break;
+        case 'dodecagon':
+            for (let i = 0; i < 12; i++) {
+                rhomb30(0, 0, 0, secondX[i], secondY[i]);
+            }
+            break;
+    }
+}
+
+function draw() {
+    output.lineRound();
+    output.fillCanvasBackgroundColor();
+    output.correctYAxis();
+    // either draw borders or some tile-design
+    tiling.drawBorders = false;
+    if (tiling.decoration !== 'none') {
+        tile();
+    }
+    if (tiling.hyperBorder || tiling.border || tiling.marker) {
+        tiling.drawBorders = true;
+        tile();
+    }
+    output.drawGrid();
+}
+
+output.setDrawMethods(draw);
+output.backgroundColorController.setValue('#eeeeee');
+
+draw();
