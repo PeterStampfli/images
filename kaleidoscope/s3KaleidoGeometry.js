ce0aac7399d113b12a450e07404b4fa0de4e5104
PeterStampfli <pestampf@gmail.com>
2021-05-18 14:04:09 +0200

    s3kaleido

---
+++ kaleidoscope/s3KaleidoGeometry.js
@@ -0,0 +1,46 @@
+/* jshint esversion: 6 */
+
+import {
+    map,
+    log
+} from "../libgui/modules.js";
+
+/**
+ * @namespace geometry
+ */
+export const geometry = {};
+
+// constants
+const eps = 0.001;
+const eps2 = eps * eps;
+const pi = Math.PI;
+const toDeg = 180 / pi;
+const fromDeg = 1 / toDeg;
+// abreviations for functions
+const cos = Math.cos;
+const sin = Math.sin;
+const tan = Math.tan;
+const round = Math.round;
+const sqrt = Math.sqrt;
+const abs = Math.abs;
+// trigonometry
+const rt3 = 1.732050808;
+const tanPi10 = Math.tan(pi / 10);
+const tan3Pi10 = Math.tan(3 * pi / 10);
+const cosPi5 = Math.cos(pi / 5);
+const sinPi5 = Math.sin(pi / 5);
+const cos2Pi5 = Math.cos(2 * pi / 5);
+const sin2Pi5 = Math.sin(2 * pi / 5);
+const cos3Pi5 = Math.cos(3 * pi / 5);
+const sin3Pi5 = Math.sin(3 * pi / 5);
+const cos4Pi5 = Math.cos(4 * pi / 5);
+const sin4Pi5 = Math.sin(4 * pi / 5);
+
+// dihedral groups and mirrors
+geometry.d12 = 5;
+geometry.d13 = 3;
+geometry.d23 = 2;
+geometry.d14 = 2;
+geometry.d24 = 2;
+geometry.d34 = 3;
+geometry.tiling='ikosahedral 533';
---
+++ kaleidoscope/s3KaleidoUI.js
@@ -0,0 +1,101 @@
+/* jshint esversion: 6 */
+
+import {
+    map,
+    output,
+    ParamGui,
+    Pixels,
+    BooleanButton,
+    Logger
+} from "../libgui/modules.js";
+
+import {
+    geometry
+} from "./s3KaleidoGeometry.js";
+
+export const controllers = {};
+
+Logger.spacing = 2;
+
+// setting up the canvas and its gui
+const gui = new ParamGui({
+    name: 'spheres',
+    closed: false
+});
+// create an output canvas, with coordinates and pixels
+output.createCanvas(gui, true);
+output.addCoordinateTransform(false);
+output.setInitialCoordinates(0, 0, 3);
+output.createPixels();
+// add options for the output image
+output.addImageProcessing();
+output.addAntialiasing();
+output.grid.interval = 0.1;
+output.addGrid();
+output.addCursorposition();
+// options of what to show
+map.makeShowingGui(gui, {
+    closed: false
+});
+map.lightController.destroy();
+map.linewidthController.destroy();
+map.trajectoryOnOffController.destroy();
+map.trajectoryColorController.destroy();
+// add drag and drop for the input image
+map.imageController.addDragAndDropWindow();
+// link the output drawing routines to the map routines
+map.setOutputDraw();
+
+// for debugging
+map.allImageControllersHide();
+
+gui.add({
+    type: 'selection',
+    params: geometry,
+    property: 'tiling',
+    options: ['ikosahedral 533', 'octahedral 433', 'tetrahedral 333', 'special 343', 'irregular 333'],
+    onChange: function(choice) {
+        console.log((choice));
+        switch (choice) {
+            case 'ikosahedral 533':
+                geometry.d12 = 5;
+                geometry.d13 = 2;
+                geometry.d23 = 3;
+                geometry.d24 = 2;
+                geometry.d34 = 3;
+                geometry.d14 = 2;
+                break;
+            case 'octahedral 433':
+                geometry.d12 = 4;
+                geometry.d13 = 2;
+                geometry.d23 = 3;
+                geometry.d24 = 2;
+                geometry.d34 = 3;
+                geometry.d14 = 2;
+                break;
+            case 'tetrahedral 333':
+                geometry.d12 = 3;
+                geometry.d13 = 2;
+                geometry.d23 = 3;
+                geometry.d24 = 2;
+                geometry.d34 = 3;
+                geometry.d14 = 2;
+                break;
+            case 'special 343':
+                geometry.d12 = 4;
+                geometry.d13 = 2;
+                geometry.d14 = 3;
+                geometry.d23 = 3;
+                geometry.d24 = 2;
+                geometry.d34 = 2;
+                break;
+            case 'irregular 333':
+                geometry.d12 = 3;
+                geometry.d13 = 3;
+                geometry.d14 = 3;
+                geometry.d23 = 2;
+                geometry.d24 = 2;
+                geometry.d34 = 2;
+        }
+    }
+});
