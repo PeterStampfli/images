c4657c0c4154253ba7222da1d74b7ef192da6beb
PeterStampfli <pestampf@gmail.com>
2021-11-30 21:19:16 +0100

    optimaziting documenting polygon to circle

--- matlabKaleidoscope/polygonToCircle.c
+++ matlabKaleidoscope/polygonToCircle.c
@@ -36,6 +36,8 @@ void mexFunction( int nlhs, mxArray *plhs[],
     bool returnsMap = false;
     int nCorners;
     float sines[200],cosines[200];
+    float dAngle, piNCorners, i2PINCorners;
+    float cosPiNCorners, sinPiNCorners, tanPiNCorners;
     /* check for proper number of arguments (else crash)*/
     /* checking for presence of a map*/
     if(nrhs != 2) {
@@ -43,15 +45,15 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
     /* check number of dimensions of the map*/
     if(mxGetNumberOfDimensions(prhs[0]) !=3 ) {
-        mexErrMsgIdAndTxt("transformMap:mapDims","The map has to have three dimensions.");
+        mexErrMsgIdAndTxt("polygonToCircle:mapDims","The map has to have three dimensions.");
     }
     dims = mxGetDimensions(prhs[0]);
     if(dims[2] != 3) {
-        mexErrMsgIdAndTxt("transformMap:map3rdDimension","The map's third dimension has to be three.");
+        mexErrMsgIdAndTxt("polygonToCircle:map3rdDimension","The map's third dimension has to be three.");
     }
     /* check that no or one output is expected*/
     if (nlhs > 1) {
-        mexErrMsgIdAndTxt("transformMap:nlhs","Has zero or one return parameter.");
+        mexErrMsgIdAndTxt("polygonToCircle:nlhs","Has zero or one return parameter.");
     }
     /* get the map*/
 #if MX_HAS_INTERLEAVED_COMPLEX
@@ -73,16 +75,19 @@ void mexFunction( int nlhs, mxArray *plhs[],
     }
     /* prepare things*/
     nCorners = (int) mxGetScalar(prhs[1]);
-    float dAngle, piN;
-    float cosPiN, sinPiN, tanPiN;
-    piN=PI/nCorners;
-    cosPiN=cosf(piN);
-    sinPiN=sinf(piN);
-    tanPiN=tanf(piN);
+    if (nCorners < 3){
+         mexErrMsgIdAndTxt("polygonToCircle:nCorners","The number of corners has to be larger than 2.");
+   }
+    piNCorners = PI/nCorners;
+    i2PiNCorners = 0.5 / piNCorners;
+    cosPiNCorners = cosf(piNCorners);
+    sinPiNCorners = sinf(piNCorners);
+    tanPiNCorners = tanf(piNCorners);
     /* the rotations of the dihedral group, order k*/
     if (nCorners > 100){
         mexErrMsgIdAndTxt("poincareDiscMapping:nCorners","Initialization, nCorners has to be smaller or equal to 100.");
     }
+    /* generate rotation"matrix"*/
     dAngle = 2.0f * PI / nCorners;
     int nCorners2 = 2 * nCorners;
     for (int i = 0; i < nCorners2; i++){
@@ -103,12 +108,19 @@ void mexFunction( int nlhs, mxArray *plhs[],
                 /* set element only if new output map*/
                 outMap[index] = INVALID;
                 outMap[index + nXnY] = INVALID;
-                outMap[index + nXnY2] = INVALID;           }
+                outMap[index + nXnY2] = INVALID;      
+            }
             continue;
         }
         x = inMap[index];
         y = inMap[index + nXnY];
-        /* do some transformation*/
+/* first find rotation by multiples of 2PI/nCorners such that */
+        /* the point is inside a sector at angles of +/- PI/nCorners to the negative y-axis*/
+        /* this sector is defined by limiting angles -PI/2-PI/nCorners and -PI/2+PI/nCorners
+        /* the angle of point (x,y) to the lower limiting y-axis is atan2(y,x) + PI/2 + PI/nCorners + 2 PI */
+        /* with a range of 3PI/2 + PI/nCorners ... 7PI/2 + PI/nCorners */
+        /* dividing by 2PI/nCorners gives the index m to the rotation "matrices", rounding down */
+        /* m=atan2(y,x)*(nCorners/2PI) + 0.5 +nCorners*(5/4) */
         
         outMap[index] = x;
         outMap[index + nXnY] = y;
