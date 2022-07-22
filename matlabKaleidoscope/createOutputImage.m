eef4b89d8ca524bd674eaedec4c45f71f64bccbd
PeterStampfli <pestampf@gmail.com>
2022-07-22 14:55:50 +0200

    createOutputImage

---
+++ matlabKaleidoscope/createOutputImage.m
@@ -0,0 +1,27 @@
+% createOutputImage: create an image using a map and an input image
+% outputImage = createOutputImage(map, inputImage);
+% invalid pixels will have black color
+% use imshow(outputImage) to show the image
+% use imwrite(outputImage,'imageName.jpg'); to save the image in the
+% current folder
+
+function outputImage = createOutputImage(map,inputImage)
+    % determine image sizes
+    [inputHeight, inputWidth, imageLayers] = size(inputImage);
+    [outputHeight, outputWidth] = size(map, [1, 2]);
+    % determine scaling to fit the map to the input image
+    [xMin, xMax, yMin, yMax] = getRangeMap(map);
+    mapWidth = xMax - xMin;
+    mapHeight = yMax - yMin;
+    scale = single(min((inputWidth - 1) / mapWidth, (inputHeight - 1) / mapHeight));
+    % do the mapping
+    offsetX = 1 - scale * xMin;
+    offsetY = 1 - scale * yMin;
+    mapX = scale * (map(:,:,1) - xMin) + 1;
+    mapY = scale * (map(:,:,2) - yMin) + 1;
+    % create the output image
+    outputImage = zeros(outputHeight, outputWidth, imageLayers, 'uint8');
+    for k = 1:imageLayers 
+        outputImage(:,:,k)  = uint8(interp2(single(inputImage(:,:,k)), mapX, mapY, 'linear', 0)); 
+    end     
+end
---
+++ matlabKaleidoscope/testRosetteImage.m
@@ -0,0 +1,21 @@
+function testRosetteImage()
+% test of the basic kaleidoscope
+% depending on symmetry paraameters k, n and m
+% shows pattern of inversions
+
+s = 1000;
+mPix=s*s/1e6;
+range=2;
+map=createIdentityMap(mPix,-range,range,-range,range);
+a=45;
+a=deg2rad(a)
+rosette(map,20,a,0,0);
+%basicKaleidoscope(map,5,3,2);
+
+inputImage = imread("dionisos.jpeg");
+
+outputImage = createOutputImage(map,inputImage);
+
+imshow(outputImage);
+%imwrite(outputImage,'image.jpg');
+end
