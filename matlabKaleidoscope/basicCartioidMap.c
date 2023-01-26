3dee81d6f4c5f6aaaa7a92a0211cc33aec01002c
PeterStampfli <pestampf@gmail.com>
2023-01-26 18:14:35 +0100

    cartioid

---
+++ matlabKaleidoscope/basicCartioidMap.c
@@ -0,0 +1,103 @@
+/*==========================================================
+ * transform cartioid to cercle
+ * Input: the map has for each pixel (h,k):
+ * map(h,k,0) = x, map(h,k,1) = y, map(h,k,2) = 0 (number of inversions)
+ *
+ * and parameter a,  a=>0 corresponds to cercle
+ *
+ * modifies the map, returns nothing if used as a procedure
+ * transform(map, ...);
+ * does not change the map and returns a modified map if used as  a function
+ * newMap = transform(map, ....);
+ *
+ *========================================================*/
+
+#include "mex.h"
+#include <math.h>
+#include <stdbool.h>
+#define PRINTI(n) printf(#n " = %d\n", n)
+#define PRINTF(n) printf(#n " = %f\n", n)
+#define INVALID -1000
+
+void mexFunction( int nlhs, mxArray *plhs[],
+        int nrhs, const mxArray *prhs[])
+{
+    const mwSize *dims;
+    int nX, nY, nXnY, nXnY2, index;
+    float inverted, x, y;
+    float *inMap, *outMap;
+    bool returnsMap = false;
+    float a, r, phi;
+    /* check for proper number of arguments (else crash)*/
+    /* checking for presence of a map*/
+    if(nrhs == 0) {
+        mexErrMsgIdAndTxt("transformMap:nrhs","A map input required.");
+    }
+    /* check number of dimensions of the map*/
+    if(mxGetNumberOfDimensions(prhs[0]) !=3 ) {
+        mexErrMsgIdAndTxt("transformMap:mapDims","The map has to have three dimensions.");
+    }
+    dims = mxGetDimensions(prhs[0]);
+    if(dims[2] != 3) {
+        mexErrMsgIdAndTxt("transformMap:map3rdDimension","The map's third dimension has to be three.");
+    }
+    /* check that no or one output is expected*/
+    if (nlhs > 1) {
+        mexErrMsgIdAndTxt("transformMap:nlhs","Has zero or one return parameter.");
+    }
+    /* get the map*/
+#if MX_HAS_INTERLEAVED_COMPLEX
+    inMap = mxGetSingles(prhs[0]);
+#else
+    inMap = (float *) mxGetPr(prhs[0]);
+#endif
+    if (nlhs == 0){
+        outMap = inMap;
+    } else {
+        /* create output map*/
+        returnsMap = true;
+        plhs[0]=mxCreateNumericArray(3, dims, mxSINGLE_CLASS, mxREAL);
+#if MX_HAS_INTERLEAVED_COMPLEX
+        outMap = mxGetSingles(plhs[0]);
+#else
+        outMap = (float *) mxGetPr(plhs[0]);
+#endif
+    }
+    // the interpolation parameter
+    if (nrhs >= 2){
+        a = fmaxf(0.001, (float) mxGetScalar(prhs[1]));
+    } else {
+        a = 1.0f;
+    }
+
+    /* do the map*/
+    /* row first order*/
+    nX = dims[1];
+    nY = dims[0];
+    nXnY = nX * nY;
+    nXnY2 = 2 * nXnY;
+    for (index = 0; index < nXnY; index++){
+        inverted = inMap[index + nXnY2];
+        /* do only transform if pixel is valid*/
+        if (inverted < -0.1f) {
+            if (returnsMap){
+                /* set element only if new output map*/
+                outMap[index] = INVALID;
+                outMap[index + nXnY] = INVALID;
+                outMap[index + nXnY2] = INVALID;           }
+            continue;
+        }
+        /* symmetry with respect to y-axis*/
+        /* 1-i a z*/
+        y =  inMap[index];
+        x = -inMap[index+nXnY];
+        /* square root*/
+        phi = 0.5 * atan2f(y,x);
+        r = sqrtf(sqrtf(y*y+x*x));
+        x = r * cosf(phi);
+        y = r * sinf(phi);
+        outMap[index] = x-a;
+        outMap[index + nXnY] = y;
+        outMap[index + nXnY2] = inverted;
+    }
+}
---
+++ matlabKaleidoscope/testCartioidKaleidoscope.m
@@ -0,0 +1,26 @@
+% creates map
+% shows the time required for the kaleidoscopic map
+% creates an image (takes a lot of time initially)
+
+% first use compile.m to compile the files
+% createIdentityMap.c, basicKaleidoscope.c and createStructureImage.c
+% do
+% >>compile;
+% >>  map = testBasicKaleidoscope(5,4,2);
+
+function testCartioidKaleidoscope(r)
+% test of the basic kaleidoscope and bulatov band projection
+% depending on symmetry paraameters k, n and m
+% interpolation a in [0,1]
+% shows pattern of inversions
+
+s = 1000;
+mPix=s*s/1e6;
+
+map=createIdentityMap(mPix,-r,r,-r,r);
+%cartioidMap(map, a);
+basicCartioidMap(map);
+outMap = basicKaleidoscope(map,5,3,3);
+im=createStructureImage(outMap);
+imshow(im);
+end
