02cfb2d18ad38e4ff5d404d296fb4c3126b4bbf3
PeterStampfli <pestampf@gmail.com>
2023-04-09 21:39:41 +0200

    julia+ continues points

--- kaleidoscope(builder)/julia.js
+++ kaleidoscope(builder)/julia.js
@@ -5,6 +5,11 @@ import {
     ParamGui
 } from "../libgui/modules.js";
 
+import {
+    Point,
+    points
+} from "./points.js";
+
 export const julia = {};
 
 function setup() {
@@ -25,19 +30,95 @@ function setup() {
     output.grid.interval = 0.1;
     output.addGrid();
     output.addCursorposition();
+
+    points.setup(gui);
+
+
+    // mouse controls
+    // mouse move with ctrl shows objects that can be selected
+    // does not need redrawing
+    output.mouseCtrlMoveAction = function(event) {
+        if (points.isSelected(event)) {
+            output.canvas.style.cursor = "pointer";
+        } else {
+            output.canvas.style.cursor = "default";
+        }
+    };
+
+    // smooth transition when ctrl key is pressed
+    output.ctrlKeyDownAction = function(event) {
+        output.mouseCtrlMoveAction(event);
+    };
+
+    // mouse down with ctrl selects intersection or circle
+    // image pixels do not change, put on canvas, draw grid&points
+    output.mouseCtrlDownAction = function(event) {
+        if (points.isSelected(event)) {
+            points.select(event);
+        }
+
+        julia.drawNoChange();
+
+    };
+
+    // changes also image
+    // mouse drag with ctrl moves selected circle
+    output.mouseCtrlDragAction = function(event) {
+        points.drag(event);
+
+        julia.drawNewStructure();
+    };
+
+    // re-drawing
+
+    // changing the grid
+    // image pixels do not change, put on canvas, draw grid&points
+    output.drawGridChanged = function() {
+
+        julia.drawNoChange();
+    };
+
+    // moving/zooming canvas
+    output.drawCanvasChanged = julia.drawNewStructure;
+
 }
 
 
 
-
-output.drawGridChanged = function() {
-
-        output.fillCanvasBackgroundColor();
-
+// structure does not change
+// image may change (other quality, input image)
+julia.drawNewImage = function() {
+    output.fillCanvasBackgroundColor();
     output.drawGrid();
+    points.draw();
 };
 
+// structure changes
+// image may change (other quality, input image)
+julia.drawNewStructure = function() {
+    output.fillCanvasBackgroundColor();
+    output.drawGrid();
+    points.draw();
+};
 
-
+// structure and image does not change
+// grid may change, selection of points may change
+julia.drawNoChange = function() {
+    output.fillCanvasBackgroundColor();
+    output.drawGrid();
+    points.draw();
+};
 
 setup();
+
+
+const a = new Point(0, 0, Point.zero);
+const b = new Point(0.5, 0.5, Point.singularity);
+const c = new Point(0, 0.5, Point.neutral);
+
+points.add(a);
+points.add(b);
+points.add(c);
+
+
+julia.drawNewStructure();
--- kaleidoscope(builder)/points.js
+++ kaleidoscope(builder)/points.js
@@ -2,9 +2,7 @@
 
 import {
     guiUtils,
-    BooleanButton,
-    output,
-    map
+    output
 }
 from "../libgui/modules.js";
 
@@ -21,6 +19,7 @@ fill[1] = '#ffff00';
 fill[2] = '#999999';
 
 // a point object that can be selected and drawn
+// inverted y-axis
 export const Point = function Point(x, y, type) {
     this.x = x;
     this.y = y;
@@ -36,11 +35,11 @@ Point.prototype.isSelected = function(position) {
     let selectionRadius2 = selectionRadius * output.coordinateTransform.totalScale;
     selectionRadius2 = selectionRadius2 * selectionRadius2;
     const dx = position.x - this.x;
-    const dy = position.y - this.y;
+    const dy = position.y + this.y;
     return (dx * dx + dy * dy) < selectionRadius2;
 };
 
-// draw the point, depending on wether is selected
+// draw the point, depending on whether is selected
 Point.prototype.draw = function(isSelected) {
     const context = output.canvasContext;
     const radius = drawRadius * output.coordinateTransform.totalScale;
@@ -52,8 +51,15 @@ Point.prototype.draw = function(isSelected) {
     }
     context.fillStyle = fill[this.type];
     context.beginPath();
-    context.arc(this.x, this.y, radius, 0, 2 * Math.PI);
+    context.arc(this.x, -this.y, radius, 0, 2 * Math.PI);
+    context.fill();
     context.stroke();
+};
+
+// drag a point
+Point.prototype.drag = function(event) {
+    this.x += event.dx;
+    this.y -= event.dy;
 };
 
 // collecting the points and doing the main work
@@ -72,9 +78,8 @@ points.add = function(point) {
 };
 
 // check if one of the points is selected, position or event (x,y) fields
-point.isSelected = function(position) {
-    const length = points.collection.length;
-    for (let i = 0; i < length; i++) {
+points.isSelected = function(position) {
+    for (let i = points.collection.length - 1; i >= 0; i--) {
         if (points.collection[i].isSelected(position)) {
             return true;
         }
@@ -83,7 +88,85 @@ point.isSelected = function(position) {
 };
 
 // select the point corresponding to event position, move it to top
+// (if not at top)
+points.select = function(position) {
+    const length = points.collection.length;
+    for (let i = length - 2; i >= 0; i--) {
+        if (points.collection[i].isSelected(position)) {
+            const selectedPoint = points.collection[i];
+            for (let j = i; j < length - 1; j++) {
+                points.collection[j] = points.collection[j + 1];
+            }
+            points.collection[length - 1] = selectedPoint;
+            points.setTop(selectedPoint);
+        }
+    }
+};
 
 // draw points
+points.draw = function() {
+    const length = points.collection.length;
+    if (length > 0) {
+        for (let i = 0; i < length - 1; i++) {
+            points.collection[i].draw(false);
+        }
+        points.collection[length - 1].draw(true);
+    }
+};
 
-// destroy selected point, at top
+// remove selected point, at top
+points.remove = function() {
+    points.collection.pop();
+};
+
+// drag selected point at top
+points.drag = function(event) {
+    const length = points.collection.length;
+    if (length > 0) {
+        points.collection[length - 1].drag(event);
+    }
+};
+
+// UI
+
+const top = {};
+top.x = 0;
+top.y = 0;
+top.type = Point.zero;
+
+points.setTop = function(point) {
+    points.topXController.setValueOnly(point.x);
+    points.topYController.setValueOnly(point.y);
+    points.topTypeController.setValueOnly(point.type);
+};
+
+points.setup = function(gui) {
+    gui.addParagraph('zeros and singularity');
+    points.topXController = gui.add({
+        type: 'number',
+        params: top,
+        property: 'x',
+        labelText: 'position',
+        onChange: function() {}
+    });
+    points.topYController = points.topXController.add({
+        type: 'number',
+        params: top,
+        property: 'y',
+        labelText: '',
+        onChange: function() {}
+    });
+    points.topTypeController = gui.add({
+        type: 'selection',
+        params: top,
+        property: 'type',
+        options: {
+            zero: Point.zero,
+            singularity: Point.singularity,
+            neutral: Point.neutral
+        },
+        onChange: function() {}
+    });
+
+
+};
