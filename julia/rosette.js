c29868e10b9d954527882cbe2d3a10e2ed3c3be9
PeterStampfli <pestampf@gmail.com>
2023-06-06 22:04:55 +0200

    rosette is now in julia

---
+++ julia/rosette.js
@@ -0,0 +1,98 @@
+/* jshint esversion:6 */
+
+import {
+    map,
+    julia
+} from "./mapImage.js";
+
+export const rosette = {};
+rosette.order = 5;
+rosette.type = rosette.nothing;
+rosette.rPower = 2;
+rosette.alpha = 1;
+
+rosette.setup = function(gui) {
+    gui.addParagraph('<strong>rosette</strong>');
+    gui.add({
+        type: 'number',
+        params: rosette,
+        property: 'order',
+        min: 2,
+        step: 1,
+        onChange: julia.drawNewStructure
+    }).add({
+        type: 'number',
+        params: rosette,
+        property: 'rPower',
+        min: 0,
+        onChange: julia.drawNewStructure
+    }).add({
+        type: 'number',
+        params: rosette,
+        property: 'alpha',
+        min: 0.01,
+        onChange: julia.drawNewStructure
+    });
+    rosette.type = rosette.nothing;
+    gui.add({
+        type: 'selection',
+        params: rosette,
+        property: 'type',
+        options: {
+            nothing: rosette.nothing,
+            'mirror symmetric': rosette.mirrorSymmetric,
+            cyclic: rosette.cyclic,
+        },
+        onChange: julia.drawNewStructure
+    });
+};
+
+rosette.nothing = function() {};
+
+rosette.mirrorSymmetric = function() {
+    const order = rosette.order;
+    const rPower = rosette.rPower / 2;
+    const iAlpha = 1 / rosette.alpha;
+    const xArray = map.xArray;
+    const yArray = map.yArray;
+    const structureArray = map.structureArray;
+    const nPixels = xArray.length;
+    for (let index = 0; index < nPixels; index++) {
+        let structure = structureArray[index];
+        /* do only transform if pixel is valid*/
+        if (structure < 128) {
+            let x = xArray[index];
+            let y = yArray[index];
+            const phi = order * Math.atan2(y, x);
+            const r2 = x * x + y * y;
+            let r = Math.pow(r2, rPower);
+            r = r / (r + iAlpha);
+            xArray[index] = r * Math.cos(phi);
+            yArray[index] = r * Math.cos(phi + phi);
+        }
+    }
+}; 
+
+rosette.cyclic = function() {
+    const order = rosette.order;
+    const rPower = rosette.rPower / 2;
+    const iAlpha = 1 / rosette.alpha;
+    const xArray = map.xArray;
+    const yArray = map.yArray;
+    const structureArray = map.structureArray;
+    const nPixels = xArray.length;
+    for (let index = 0; index < nPixels; index++) {
+        let structure = structureArray[index];
+        /* do only transform if pixel is valid*/
+        if (structure < 128) {
+            let x = xArray[index];
+            let y = yArray[index];
+            const phi = order * Math.atan2(y, x);
+            const r2 = x * x + y * y;
+            let r = Math.pow(r2, rPower);
+            r = r / (r + iAlpha);
+            xArray[index] = r * Math.cos(phi);
+            yArray[index] = r * Math.sin(phi);
+        }
+    }
+};
---
+++ julia/rosetteSetup.js
@@ -0,0 +1,69 @@
+/* jshint esversion: 6 */
+
+import {
+    output,
+    ParamGui
+} from "../libgui/modules.js";
+
+import {
+    rosette
+} from "./rosette.js";
+
+import {
+    map,julia
+} from "./mapImage.js";
+
+function setup() {
+    // base gui
+    const gui = new ParamGui({
+        name: 'julia+',
+        closed: false
+    });
+    julia.gui = gui;
+
+    output.createCanvas(gui, true);
+    output.addCoordinateTransform(false);
+    output.setInitialCoordinates(0, 0, 2);
+    output.createPixels();
+    output.grid.interval = 0.1;
+    output.addGrid();
+    output.addCursorposition();
+    rosette.setup(gui);
+    map.setupDrawing(gui);
+
+    // changing the grid
+    // image pixels do not change, put on canvas, draw grid&points
+    output.drawGridChanged = function() {
+        julia.drawNoChange();
+    };
+
+    // moving/zooming canvas
+    output.drawCanvasChanged = julia.drawNewStructure;
+}
+
+// structure does not change
+// (input) image may change (other quality, input image)
+julia.drawNewImage = function() {
+    map.draw();
+    output.drawGrid();
+};
+
+// structure changes
+// image may change (other quality, input image)
+julia.drawNewStructure = function() {
+    map.init();
+    rosette.type();
+    map.draw();
+    output.drawGrid();
+};
+
+// structure and image does not change
+// grid may change, selection of points may change
+julia.drawNoChange = function() {
+    map.draw();
+    output.drawGrid();
+};
+
+setup();
+
+julia.drawNewStructure();
