0476586092b13915a7103d39ff01f4e6aa085c25
PeterStampfli <pestampf@gmail.com>
2024-06-03 20:30:53 +0200

    modified:   experiments/waves.js

--- experiments/waves.js
+++ experiments/waves.js
@@ -15,6 +15,8 @@ waves.scale = 1;
 waves.transX = 0;
 waves.transY = 0;
 waves.structureOfX = true;
+waves.brokenX = false;
+waves.brokenY = false;
 
 // the different wave packages
 
@@ -72,28 +74,115 @@ waves.regular = function() {
             sines[n] = Math.sin(n * dAngle);
             cosines[n] = Math.cos(n * dAngle);
         }
-        for (let index = 0; index < nPixels; index++) {
-            let x = xArray[index];
-            let y = yArray[index];
-            let xNew = 0;
-            let yNew = 0;
-            let projection = x + offset;
-            for (let n = 1; n < m; n++) {
-                const nextProjection = x * cosines[n] + y * sines[n] + offset;
-                xNew += Math.cos(projection);
-                yNew += Math.cos(projection + nextProjection);
-                projection = nextProjection;
-            }
-            xNew += Math.cos(projection);
-            yNew += Math.cos(projection - x);
-            xNew = scale * xNew + transX;
-            yNew = scale * yNew + transY;
-            xArray[index] = xNew;
-            yArray[index] = yNew;
-            if (structureOfX) {
-                structureArray[index] = (xNew > 0) ? 1 : 0;
+        if (waves.brokenX) {
+            if (waves.brokenY) {
+                for (let index = 0; index < nPixels; index++) {
+                    let x = xArray[index];
+                    let y = yArray[index];
+                    let xNew = 0;
+                    let yNew = 0;
+                    let sign = 1;
+                    let projection = x + offset;
+                    for (let n = 1; n < m; n++) {
+                        const nextProjection = x * cosines[n] + y * sines[n] + offset;
+                        xNew += sign * Math.cos(projection);
+                        yNew += sign * Math.cos(projection + nextProjection);
+                        projection = nextProjection;
+                        sign = -sign;
+                    }
+                    xNew += sign * Math.cos(projection);
+                    yNew += sign * Math.cos(projection - x);
+                    xNew = scale * xNew + transX;
+                    yNew = scale * yNew + transY;
+                    xArray[index] = xNew;
+                    yArray[index] = yNew;
+                    if (structureOfX) {
+                        structureArray[index] = (xNew > 0) ? 1 : 0;
+                    } else {
+                        structureArray[index] = (yNew > 0) ? 1 : 0;
+                    }
+                }
             } else {
-                structureArray[index] = (yNew > 0) ? 1 : 0;
+                for (let index = 0; index < nPixels; index++) {
+                    let x = xArray[index];
+                    let y = yArray[index];
+                    let xNew = 0;
+                    let yNew = 0;
+                    let sign = 1;
+                    let projection = x + offset;
+                    for (let n = 1; n < m; n++) {
+                        const nextProjection = x * cosines[n] + y * sines[n] + offset;
+                        xNew += sign * Math.cos(projection);
+                        yNew += Math.cos(projection + nextProjection);
+                        projection = nextProjection;
+                        sign = -sign;
+                    }
+                    xNew += sign * Math.cos(projection);
+                    yNew += Math.cos(projection - x);
+                    xNew = scale * xNew + transX;
+                    yNew = scale * yNew + transY;
+                    xArray[index] = xNew;
+                    yArray[index] = yNew;
+                    if (structureOfX) {
+                        structureArray[index] = (xNew > 0) ? 1 : 0;
+                    } else {
+                        structureArray[index] = (yNew > 0) ? 1 : 0;
+                    }
+                }
+            }
+        } else {
+            if (waves.brokenY) {
+                for (let index = 0; index < nPixels; index++) {
+                    let x = xArray[index];
+                    let y = yArray[index];
+                    let xNew = 0;
+                    let yNew = 0;
+                    let sign = 1;
+                    let projection = x + offset;
+                    for (let n = 1; n < m; n++) {
+                        const nextProjection = x * cosines[n] + y * sines[n] + offset;
+                        xNew += Math.cos(projection);
+                        yNew += sign * Math.cos(projection + nextProjection);
+                        projection = nextProjection;
+                        sign = -sign;
+                    }
+                    xNew += Math.cos(projection);
+                    yNew += sign * Math.cos(projection - x);
+                    xNew = scale * xNew + transX;
+                    yNew = scale * yNew + transY;
+                    xArray[index] = xNew;
+                    yArray[index] = yNew;
+                    if (structureOfX) {
+                        structureArray[index] = (xNew > 0) ? 1 : 0;
+                    } else {
+                        structureArray[index] = (yNew > 0) ? 1 : 0;
+                    }
+                }
+            } else {
+   for (let index = 0; index < nPixels; index++) {
+                    let x = xArray[index];
+                    let y = yArray[index];
+                    let xNew = 0;
+                    let yNew = 0;
+                    let projection = x + offset;
+                    for (let n = 1; n < m; n++) {
+                        const nextProjection = x * cosines[n] + y * sines[n] + offset;
+                        xNew +=  Math.cos(projection);
+                        yNew +=  Math.cos(projection + nextProjection);
+                        projection = nextProjection;
+                    }
+                    xNew +=  Math.cos(projection);
+                    yNew +=  Math.cos(projection - x);
+                    xNew = scale * xNew + transX;
+                    yNew = scale * yNew + transY;
+                    xArray[index] = xNew;
+                    yArray[index] = yNew;
+                    if (structureOfX) {
+                        structureArray[index] = (xNew > 0) ? 1 : 0;
+                    } else {
+                        structureArray[index] = (yNew > 0) ? 1 : 0;
+                    }
+                }
             }
         }
     }
@@ -224,7 +313,57 @@ waves.sumOfSines = function() {
                 structureArray[index] = (yNew > 0) ? 1 : 0;
             }
         }
-    } 
+    }
+};
+
+
+waves.productOfSines = function() {
+    const symmetry = waves.symmetry;
+    const offset = waves.offset;
+    const scale = waves.scale;
+    const transX = waves.transX;
+    const transY = waves.transY;
+    const structureOfX = waves.structureOfX;
+    const sines = [];
+    const cosines = [];
+    var dAngle;
+    const xArray = map.xArray;
+    const nPixels = xArray.length;
+    const yArray = map.yArray;
+    const structureArray = map.structureArray;
+    if ((symmetry & 1) !== 0) {
+        sines.length = symmetry;
+        cosines.length = symmetry;
+        dAngle = 2 * Math.PI / symmetry;
+        for (let n = 0; n < symmetry; n++) {
+            sines[n] = Math.sin(n * dAngle);
+            cosines[n] = Math.cos(n * dAngle);
+        }
+        for (let index = 0; index < nPixels; index++) {
+            let x = xArray[index];
+            let y = yArray[index];
+            let xNew = 1;
+            let yNew = 1;
+            let projection = x + offset;
+            for (let n = 1; n < symmetry; n++) {
+                const nextProjection = x * cosines[n] + y * sines[n] + offset;
+                xNew *= Math.sin(projection);
+                yNew *= Math.sin(projection + nextProjection);
+                projection = nextProjection;
+            }
+            xNew *= Math.sin(projection);
+            yNew *= Math.sin(projection + x);
+            xNew = scale * xNew + transX;
+            yNew = scale * yNew + transY;
+            xArray[index] = xNew;
+            yArray[index] = yNew;
+            if (structureOfX) {
+                structureArray[index] = (xNew > 0) ? 1 : 0;
+            } else {
+                structureArray[index] = (yNew > 0) ? 1 : 0;
+            }
+        }
+    }
 };
 
 waves.sumSimpleCos = function() {
@@ -270,6 +409,49 @@ waves.sumSimpleCos = function() {
     }
 };
 
+waves.prodSimpleCos = function() {
+    const symmetry = waves.symmetry;
+    const offset = waves.offset;
+    const scale = waves.scale;
+    const transX = waves.transX;
+    const transY = waves.transY;
+    const structureOfX = waves.structureOfX;
+    const sines = [];
+    const cosines = [];
+    var dAngle;
+    const xArray = map.xArray;
+    const nPixels = xArray.length;
+    const yArray = map.yArray;
+    const structureArray = map.structureArray;
+    sines.length = symmetry;
+    cosines.length = symmetry;
+    dAngle = 0.5 * Math.PI / symmetry;
+    let s2 = 2 * symmetry;
+    for (let n = 0; n < s2; n++) {
+        sines[n] = Math.sin(n * dAngle);
+        cosines[n] = Math.cos(n * dAngle);
+    }
+    for (let index = 0; index < nPixels; index++) {
+        let x = xArray[index];
+        let y = yArray[index];
+        let xNew = 1;
+        let yNew = 1;
+        for (let n = 0; n < s2; n += 2) {
+            xNew *= Math.cos(x * cosines[n] + y * sines[n] + offset);
+            yNew *= Math.cos(x * cosines[n + 1] + y * sines[n + 1] + offset);
+        }
+        xNew = scale * xNew + transX;
+        yNew = scale * yNew + transY;
+        xArray[index] = xNew;
+        yArray[index] = yNew;
+        if (structureOfX) {
+            structureArray[index] = (xNew > 0) ? 1 : 0;
+        } else {
+            structureArray[index] = (yNew > 0) ? 1 : 0;
+        }
+    }
+};
+
 waves.type = waves.regular;
 
 waves.setup = function(gui) {
@@ -293,7 +475,9 @@ waves.setup = function(gui) {
             'regular sum': waves.regular,
             'regular prod': waves.product,
             'sum of sines': waves.sumOfSines,
+            'prod of sines': waves.productOfSines,
             'sum simple cos': waves.sumSimpleCos,
+            'prod simple cos': waves.prodSimpleCos,
             'nothing': waves.nothing
         },
         onChange: julia.drawNewStructure
@@ -323,9 +507,21 @@ waves.setup = function(gui) {
         labelText: 'structure of x',
         onChange: julia.drawNewStructure
     });
+    base.gui.add({
+        type: 'boolean',
+        params: waves,
+        property: 'brokenX',
+        labelText: 'break x',
+        onChange: julia.drawNewStructure
+    }).add({
+        type: 'boolean',
+        params: waves,
+        property: 'brokenY',
+        labelText: 'y',
+        onChange: julia.drawNewStructure
+    });
 };
 
 waves.map = function() {
-    console.log(waves.type);
     waves.type();
 };
